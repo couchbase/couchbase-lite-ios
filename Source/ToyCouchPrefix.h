377cdfffd00b6aca646e83deab15eca192aebb02
Jens Alfke <jens@mooseyard.com>
2011-12-08 09:32:31 -0800

    Got the release (archive) build working.

---
+++ Source/ToyCouchPrefix.h
@@ -0,0 +1,9 @@
+//
+//  ToyCouchPrefix.h
+//  ToyCouch
+//
+//  Created by Jens Alfke on 12/7/11.
+//  Copyright (c) 2011 Couchbase, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
