couchbaselabs/TouchDB-iOS

Added support for filtered replication (push)

Source/ChangeTracker/TDChangeTracker.h
@@ -41,6 +41,7 @@ typedef enum TDChangeTrackerMode {
     TDChangeTrackerMode _mode;
     NSUInteger _lastSequenceNumber;
     NSString* _filterName;
+    NSDictionary* _queryParameters;
 }
 
 - (id)initWithDatabaseURL: (NSURL*)databaseURL
@@ -55,6 +56,7 @@ typedef enum TDChangeTrackerMode {
 @property (nonatomic, assign) id<TDChangeTrackerClient> client;
 
 @property (copy) NSString* filterName;
+@property (copy) NSDictionary* queryParameters;
 
 - (BOOL) start;
 - (void) stop;

Source/TDPuller.h
@@ -20,8 +20,10 @@
     NSUInteger _httpConnectionCount;
     TDBatcher* _revsToInsert;
     NSString* _filterName;
+    NSDictionary* _queryParameters;
 }
 
 @property (copy) NSString* filterName;
+@property (copy) NSDictionary* queryParameters;
 
 @end

couchbase/couchbase-lite-ios

Source/ChangeTracker/TDChangeTracker.h
@@ -42,6 +42,7 @@ typedef enum TDChangeTrackerMode {
     id _lastSequenceID;
     NSError* _error;
     NSString* _filterName;
+    NSDictionary* _filterParameters;
 }
 
 - (id)initWithDatabaseURL: (NSURL*)databaseURL
@@ -57,6 +58,7 @@ typedef enum TDChangeTrackerMode {
 @property (assign, nonatomic) id<TDChangeTrackerClient> client;
 
 @property (copy) NSString* filterName;
+@property (copy) NSDictionary* filterParameters;
 
 - (BOOL) start;
 - (void) stop;

Source/TDPuller.h
@@ -22,9 +22,11 @@
     NSUInteger _httpConnectionCount;
     TDBatcher* _revsToInsert;
     NSString* _filterName;
+    NSDictionary* _filterParameters;
 }
 
 @property (copy) NSString* filterName;
+@property (copy) NSDictionary* filterParameters;
 
 @end
 