couchbase/couchbase-lite-ios

commit c251be311d870243cafe725c016a0094e2402e88

Map function should see a _conflicts property if doc is in conflict.
Fixes #49.

Source/TDView.m
@@ -229,33 +229,44 @@ static id fromJSON( NSData* json ) {
         if (!r)
             return 500;
 
-        int64_t lastDocID = 0;
-        while ([r next]) {
+        BOOL keepGoing = [r next]; // Go to first result row
+        while (keepGoing) {
             @autoreleasepool {
+                // Reconstitute the document as a dictionary:
+                sequence = [r longLongIntForColumnIndex: 1];
+                NSString* docID = [r stringForColumnIndex: 2];
+                if ([docID hasPrefix: @"_design/"])     // design docs don't get indexed!
+                    continue;
+                NSString* revID = [r stringForColumnIndex: 3];
+                NSData* json = [r dataForColumnIndex: 4];
+            
+                // Iterate over following rows with the same doc_id -- these are conflicts.
+                // Skip them, but collect their revIDs:
                 int64_t doc_id = [r longLongIntForColumnIndex: 0];
-                if (doc_id != lastDocID) {
-                    // Only look at the first-iterated revision of any document, because this is the
-                    // one with the highest revid, hence the "winning" revision of a conflict.
-                    lastDocID = doc_id;
-                    
-                    // Reconstitute the document as a dictionary:
-                    sequence = [r longLongIntForColumnIndex: 1];
-                    NSString* docID = [r stringForColumnIndex: 2];
-                    if ([docID hasPrefix: @"_design/"])     // design docs don't get indexed!
-                        continue;
-                    NSString* revID = [r stringForColumnIndex: 3];
-                    NSData* json = [r dataNoCopyForColumnIndex: 4];
-                    NSDictionary* properties = [_db documentPropertiesFromJSON: json
-                                                                         docID: docID revID:revID
-                                                                      sequence: sequence
-                                                                       options: 0];
-                    if (properties) {
-                        // Call the user-defined map() to emit new key/value pairs from this revision:
-                        LogTo(View, @"  call map for sequence=%lld...", sequence);
-                        _mapBlock(properties, emit);
-                        if (emitFailed)
-                            return 500;
+                NSMutableArray* conflicts = nil;
+                while ((keepGoing = [r next]) && [r longLongIntForColumnIndex: 0] == doc_id) {
+                    if (!conflicts)
+                        conflicts = $marray();
+                    [conflicts addObject: [r stringForColumnIndex: 3]];
+                }
+            
+                NSDictionary* properties = [_db documentPropertiesFromJSON: json
+                                                                     docID: docID revID:revID
+                                                                  sequence: sequence
+                                                                   options: 0];
+                if (properties) {
+                    if (conflicts) {
+                        // Add a "_conflicts" property if there were conflicting revisions:
+                        NSMutableDictionary* mutableProps = [[properties mutableCopy] autorelease];
+                        [mutableProps setObject: conflicts forKey: @"_conflicts"];
+                        properties = mutableProps;
                     }
+                    
+                    // Call the user-defined map() to emit new key/value pairs from this revision:
+                    LogTo(View, @"  call map for sequence=%lld...", sequence);
+                    _mapBlock(properties, emit);
+                    if (emitFailed)
+                        return 500;
                 }
             }
         }

Source/TDView_Tests.m
@@ -148,6 +148,40 @@ TestCase(TDView_Index) {
 }
 
 
+TestCase(TDView_MapConflicts) {
+    RequireTestCase(TDView_Index);
+    TDDatabase *db = createDB();
+    NSArray* docs = putDocs(db);
+    TDRevision* leaf1 = [docs objectAtIndex: 1];
+    
+    // Create a conflict:
+    NSDictionary* props = $dict({@"_id", @"44444"},
+                                {@"_rev", @"1-~~~~~"},  // higher revID, will win conflict
+                                {@"key", @"40ur"});
+    TDRevision* leaf2 = [[[TDRevision alloc] initWithProperties: props] autorelease];
+    TDStatus status = [db forceInsert: leaf2 revisionHistory: $array() source: nil];
+    CAssert(status < 300);
+    CAssertEqual(leaf1.docID, leaf2.docID);
+    
+    TDView* view = [db viewNamed: @"conflicts"];
+    [view setMapBlock: ^(NSDictionary* doc, TDMapEmitBlock emit) {
+        NSString* docID = [doc objectForKey: @"_id"];
+        NSArray* conflicts = $cast(NSArray, [doc objectForKey: @"_conflicts"]);
+        if (conflicts) {
+            Log(@"Doc %@, _conflicts = %@", docID, conflicts);
+            emit(docID, conflicts);
+        }
+    } reduceBlock: NULL version: @"1"];
+    
+    CAssertEq([view updateIndex], 200);
+    NSArray* dump = [view dump];
+    Log(@"View dump: %@", dump);
+    CAssertEqual(dump, $array($dict({@"key", @"\"44444\""},
+                                    {@"value", $sprintf(@"[\"%@\"]", leaf1.revID)},
+                                    {@"seq", $object(6)}) ));
+}
+
+
 TestCase(TDView_Query) {
     RequireTestCase(TDView_Index);
     TDDatabase *db = createDB();

commit 3c70668ef35924c9455038773c6a59bfd34d4f6f

Fixed an infinite loop indexing views
Regression introduced by c251be31. Occurs if the db contains a design document.

Source/TDView.m
@@ -235,8 +235,10 @@ static id fromJSON( NSData* json ) {
                 // Reconstitute the document as a dictionary:
                 sequence = [r longLongIntForColumnIndex: 1];
                 NSString* docID = [r stringForColumnIndex: 2];
-                if ([docID hasPrefix: @"_design/"])     // design docs don't get indexed!
+                if ([docID hasPrefix: @"_design/"]) {     // design docs don't get indexed!
+                    keepGoing = [r next];
                     continue;
+                }
                 NSString* revID = [r stringForColumnIndex: 3];
                 NSData* json = [r dataForColumnIndex: 4];
             

Source/TDView_Tests.m
@@ -95,6 +95,7 @@ TestCase(TDView_Index) {
     TDRevision* rev1 = putDoc(db, $dict({@"key", @"one"}));
     TDRevision* rev2 = putDoc(db, $dict({@"key", @"two"}));
     TDRevision* rev3 = putDoc(db, $dict({@"key", @"three"}));
+    putDoc(db, $dict({@"_id", @"_design/foo"}));
     putDoc(db, $dict({@"clef", @"quatre"}));
     
     TDView* view = createView(db);
@@ -131,8 +132,8 @@ TestCase(TDView_Index) {
 
     dump = [view dump];
     Log(@"View dump: %@", dump);
-    CAssertEqual(dump, $array($dict({@"key", @"\"3hree\""}, {@"seq", $object(5)}),
-                              $dict({@"key", @"\"four\""}, {@"seq", $object(6)}),
+    CAssertEqual(dump, $array($dict({@"key", @"\"3hree\""}, {@"seq", $object(6)}),
+                              $dict({@"key", @"\"four\""}, {@"seq", $object(7)}),
                               $dict({@"key", @"\"one\""}, {@"seq", $object(1)}) ));
     
     // Now do a real query: