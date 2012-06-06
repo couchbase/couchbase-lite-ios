couchbase/couchbase-lite-ios

commit 45f592fd38a5fbe3c3504aeb2495d83e111102b6

Implemented linked documents for views
Support for linked documents: http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views#Linked_documents
Emitting a _id key from a map function will cause include_docs to include the doc with that ID instead.
Note: This is not the most efficient way to implement this.
Fixes #92.

Source/TDView.m
@@ -481,13 +481,23 @@ static id groupKey(id key, unsigned groupLevel) {
             } else {
                 // Regular query:
                 NSString* docID = [r stringForColumnIndex: 2];
-                NSDictionary* docContents = nil;
+                id docContents = nil;
                 if (options->includeDocs) {
-                    docContents = [_db documentPropertiesFromJSON: [r dataNoCopyForColumnIndex: 4]
-                                                            docID: docID
-                                                            revID: [r stringForColumnIndex: 3]
-                                                         sequence: [r longLongIntForColumnIndex:5]
-                                                          options: options->content];
+                    NSString* linkedID = [value objectForKey: @"_id"];
+                    if (linkedID) {
+                        // Linked document: http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views#Linked_documents
+                        NSString* linkedRev = [value objectForKey: @"_rev"]; // usually nil
+                        TDRevision* linked = [_db getDocumentWithID: linkedID
+                                                         revisionID: linkedRev
+                                                            options: options->content];
+                        docContents = linked ? linked.properties : $null;
+                    } else {
+                        docContents = [_db documentPropertiesFromJSON: [r dataNoCopyForColumnIndex: 4]
+                                                                docID: docID
+                                                                revID: [r stringForColumnIndex: 3]
+                                                             sequence: [r longLongIntForColumnIndex:5]
+                                                              options: options->content];
+                    }
                 }
                 [rows addObject: $dict({@"id",  docID},
                                        {@"key", key},

Source/TDView_Tests.m
@@ -604,4 +604,51 @@ TestCase(TDView_CollationRaw) {
 }
 
 
+TestCase(TDView_LinkedDocs) {
+    RequireTestCase(TDView_Query);
+    TDDatabase *db = createDB();
+    NSArray* revs = putDocs(db);
+    
+    NSDictionary* docs[5];
+    int i = 0;
+    for (TDRevision* rev in revs) {
+        docs[i++] = [db getDocumentWithID: rev.docID revisionID: rev.revID options: 0].properties;
+    }
+
+    TDView* view = [db viewNamed: @"linkview"];
+    [view setMapBlock: ^(NSDictionary* doc, TDMapEmitBlock emit) {
+        NSString* key = [doc objectForKey: @"key"];
+        NSDictionary* value = nil;
+        int linkedID = [[doc objectForKey: @"_id"] intValue] - 11111;
+        if (linkedID > 0)
+            value = $dict({@"_id", $sprintf(@"%d", linkedID)});
+        emit(key, value);
+    } reduceBlock: NULL version: @"1"];
+
+    CAssertEq([view updateIndex], kTDStatusOK);
+    
+    // Query all rows:
+    TDQueryOptions options = kDefaultTDQueryOptions;
+    options.includeDocs = YES;
+    TDStatus status;
+    NSArray* rows = [view queryWithOptions: &options status: &status];
+    NSArray* expectedRows = $array($dict({@"id",  @"55555"}, {@"key", @"five"},
+                                         {@"value", $dict({@"_id", @"44444"})},
+                                         {@"doc", docs[1]}),
+                                   $dict({@"id",  @"44444"}, {@"key", @"four"},
+                                         {@"value", $dict({@"_id", @"33333"})},
+                                         {@"doc", docs[3]}),
+                                   $dict({@"id",  @"11111"}, {@"key", @"one"},
+                                         {@"doc", docs[2]}),
+                                   $dict({@"id",  @"33333"}, {@"key", @"three"},
+                                         {@"value", $dict({@"_id", @"22222"})},
+                                         {@"doc", docs[0]}),
+                                   $dict({@"id",  @"22222"}, {@"key", @"two"},
+                                         {@"value", $dict({@"_id", @"11111"})},
+                                         {@"doc", docs[2]}));
+    CAssertEqual(rows, expectedRows);
+    
+}
+
+
 #endif

commit 62745dc31f25cef83556d84d87943b14534ba5c9

Fix exception in view query if row value is non-dictionary
Regression introduced by 45f592f (linked documents). Oops.

Source/TDView.m
@@ -483,7 +483,7 @@ static id groupKey(id key, unsigned groupLevel) {
                 NSString* docID = [r stringForColumnIndex: 2];
                 id docContents = nil;
                 if (options->includeDocs) {
-                    NSString* linkedID = [value objectForKey: @"_id"];
+                    NSString* linkedID = [$castIf(NSDictionary, value) objectForKey: @"_id"];
                     if (linkedID) {
                         // Linked document: http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views#Linked_documents
                         NSString* linkedRev = [value objectForKey: @"_rev"]; // usually nil