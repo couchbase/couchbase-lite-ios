couchbase/couchbase-lite-ios

commit d7fa11c406fb219b8a5cf2ebf62a3771b8b03573

Add info about active HTTP requests to _active_tasks
New task property "x_active_requests" is an array of dictionaries.
Each dict has properties "URL" and "method" that describe the request.

Source/TDRemoteRequest.m
@@ -125,6 +125,11 @@
 }
 
 
+- (NSMutableDictionary*) statusInfo {
+    return $mdict({@"URL", _request.URL}, {@"method", _request.HTTPMethod});
+}
+
+
 - (void) respondWithResult: (id)result error: (NSError*)error {
     Assert(result || error);
     _onCompletion(result, error);

Source/TDRemoteRequest.h
@@ -52,6 +52,9 @@ typedef void (^TDRemoteRequestCompletionBlock)(id result, NSError* error);
 /** Stops the request, calling the onCompletion block. */
 - (void) stop;
 
+/** JSON-compatible dictionary with status information, to be returned from _activity API */
+@property (readonly) NSMutableDictionary* statusInfo;
+
 // protected:
 - (void) setupRequest: (NSMutableURLRequest*)request withBody: (id)body;
 - (void) clearConnection;

Source/TDReplicator.h
@@ -102,4 +102,7 @@ extern NSString* TDReplicatorStoppedNotification;
     This is only an estimate and its value will change during replication. It starts at zero and returns to zero when replication stops. */
 @property (readonly, nonatomic) NSUInteger changesTotal;
 
+/** JSON-compatible array of status info about active remote HTTP requests. */
+@property (readonly) NSArray* activeRequestsStatus;
+
 @end

Source/TDReplicator.m
@@ -418,6 +418,13 @@ NSString* TDReplicatorStoppedNotification = @"TDReplicatorStopped";
 }
 
 
+- (NSArray*) activeRequestsStatus {
+    return [_remoteRequests my_map: ^id(TDRemoteRequest* request) {
+        return request.statusInfo;
+    }];
+}
+
+
 #pragma mark - CHECKPOINT STORAGE:
 
 

Source/TDRouter+Handlers.m
@@ -151,6 +151,7 @@
                                        {@"continuous", (repl.continuous ? $true : nil)},
                                        {@"status", status},
                                        {@"progress", progress},
+                                       {@"x_active_requests", repl.activeRequestsStatus},
                                        {@"error", error})];
         }
     }

commit 9e20d3fb2b52bcbaf7881014f2d39dadd2ec036f

Oops, fix exception returning _active_tasks
Stupid regression introduced earlier today by d7fa11c406fb.
Fixes #145

Source/TDRemoteRequest.m
@@ -126,7 +126,7 @@
 
 
 - (NSMutableDictionary*) statusInfo {
-    return $mdict({@"URL", _request.URL}, {@"method", _request.HTTPMethod});
+    return $mdict({@"URL", _request.URL.absoluteString}, {@"method", _request.HTTPMethod});
 }
 
 