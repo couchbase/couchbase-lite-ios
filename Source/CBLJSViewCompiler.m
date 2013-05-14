90d8e25fbfa973046922b2547088bbb7fcf7a327
Chris Anderson <jchris@couchbase.com>
2013-05-14 11:26:26 -0700

    clean up import line

--- Source/CBLJSViewCompiler.m
+++ Source/CBLJSViewCompiler.m
@@ -14,7 +14,7 @@
 
 #import "CBLJSViewCompiler.h"
 #import "CBLJSFunction.h"
-#import "CBLRevision.h"
+#import <CouchbaseLite/CBLRevision.h>
 #import <JavaScriptCore/JavaScript.h>
 #import <JavaScriptCore/JSStringRefCF.h>
 
