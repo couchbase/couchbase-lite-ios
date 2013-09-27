couchbase/couchbase-lite-ios

commit e4c2115cae3335367ef6e5709738efd1ddb6abbf

Make change-tracker not stop on errors, in continuous replication
For #108

Source/ChangeTracker/CBLSocketChangeTracker.m
@@ -25,8 +25,6 @@
 #import <string.h>
 
 
-#define kMaxRetries 6
-#define kInitialRetryDelay 0.2
 #define kReadLength 4096u
 
 
@@ -255,7 +253,7 @@
             if (_credential) {
                 // Recoverable auth failure -- close socket but try again with _credential:
                 _unauthResponse = response;
-                [self errorOccurred: CBLStatusToNSError((CBLStatus)status, self.changesFeedURL)];
+                [self failedWithError: CBLStatusToNSError((CBLStatus)status, self.changesFeedURL)];
                 return NO;
             }
         }
@@ -334,9 +332,9 @@
     } else {
         // Response data was truncated. This has been reported as an intermittent error
         // (see TouchDB issue #241). Treat it as if it were a socket error -- i.e. pause/retry.
-        [self errorOccurred: [NSError errorWithDomain: NSURLErrorDomain
-                                                 code: NSURLErrorNetworkConnectionLost
-                                             userInfo: nil]];
+        [self failedWithError: [NSError errorWithDomain: NSURLErrorDomain
+                                                   code: NSURLErrorNetworkConnectionLost
+                                               userInfo: nil]];
     }
     return YES;
 }
@@ -440,26 +438,16 @@
 }
 
 
-- (void) errorOccurred: (NSError*)error {
-    LogTo(ChangeTracker, @"%@: ErrorOccurred: %@", self, error);
-    if (++_retryCount <= kMaxRetries) {
-        [self clearConnection];
-        NSTimeInterval retryDelay = kInitialRetryDelay * (1 << (_retryCount-1));
-        [self performSelector: @selector(start) withObject: nil afterDelay: retryDelay];
-    } else {
-        Warn(@"%@: Can't connect, giving up: %@", self, error);
-
-        // Map lower-level errors from CFStream to higher-level NSURLError ones:
-        if ($equal(error.domain, NSPOSIXErrorDomain)) {
-            if (error.code == ECONNREFUSED)
-                error = [NSError errorWithDomain: NSURLErrorDomain
-                                            code: NSURLErrorCannotConnectToHost
-                                        userInfo: error.userInfo];
-        }
-
-        self.error = error;
-        [self stop];
+- (void) failedWithError:(NSError*) error {
+    // Map lower-level errors from CFStream to higher-level NSURLError ones:
+    if ($equal(error.domain, NSPOSIXErrorDomain)) {
+        if (error.code == ECONNREFUSED)
+            error = [NSError errorWithDomain: NSURLErrorDomain
+                                        code: NSURLErrorCannotConnectToHost
+                                    userInfo: error.userInfo];
     }
+    [self clearConnection];
+    [super failedWithError: error];
 }
 
 
@@ -483,9 +471,9 @@
             LogTo(ChangeTracker, @"%@: EndEncountered %@", self, stream);
             _atEOF = true;
             if (!_gotResponseHeaders || (_mode == kContinuous && _inputBuffer.length > 0)) {
-                [self errorOccurred: [NSError errorWithDomain: NSURLErrorDomain
-                                                         code: NSURLErrorNetworkConnectionLost
-                                                     userInfo: nil]];
+                [self failedWithError: [NSError errorWithDomain: NSURLErrorDomain
+                                                           code: NSURLErrorNetworkConnectionLost
+                                                       userInfo: nil]];
                 break;
             }
             if (_mode == kContinuous) {
@@ -497,7 +485,7 @@
             break;
             
         case NSStreamEventErrorOccurred:
-            [self errorOccurred: stream.streamError];
+            [self failedWithError: stream.streamError];
             break;
             
         default:

Source/CBLMisc.h
@@ -54,6 +54,10 @@ BOOL CBLIsOfflineError( NSError* error );
     Examples are timeout, connection lost, 502 Bad Gateway... */
 BOOL CBLMayBeTransientError( NSError* error );
 
+/** Returns YES if this is a network/HTTP error that should be considered permanent, i.e.
+    the problem probably lies with the local setup (wrong URL or wrong credentials.) */
+BOOL CBLIsPermanentError( NSError* error );
+
 /** Returns YES if this error appears to be due to a creating a file/dir that already exists. */
 BOOL CBLIsFileExistsError( NSError* error );
 

Source/CBLMisc.m
@@ -249,6 +249,19 @@ BOOL CBLMayBeTransientError( NSError* error ) {
 }
 
 
+BOOL CBLIsPermanentError( NSError* error ) {
+    NSString* domain = error.domain;
+    NSInteger code = error.code;
+    if ($equal(domain, NSURLErrorDomain)) {
+        return code == NSURLErrorBadURL || code == NSURLErrorUnsupportedURL;
+    } else if ($equal(domain, CBLHTTPErrorDomain)) {
+        return code >= 400 && code <= 499;
+    } else {
+        return NO;
+    }
+}
+
+
 BOOL CBLRemoveFileIfExists(NSString* path, NSError** outError) {
     NSFileManager* fmgr = [NSFileManager defaultManager];
     return [fmgr removeItemAtPath: path error: outError] || ![fmgr fileExistsAtPath: path];

Source/CBL_Puller.m
@@ -96,6 +96,7 @@ static NSString* joinQuotedEscaped(NSArray* strings);
                                                            client: self];
     // Limit the number of changes to return, so we can parse the feed in parts:
     _changeTracker.limit = kChangesFeedLimit;
+    _changeTracker.continuous = _continuous;
     _changeTracker.filterName = _filterName;
     _changeTracker.filterParameters = _filterParameters;
     _changeTracker.docIDs = _docIDs;

Source/ChangeTracker/CBLChangeTracker.h
@@ -46,6 +46,7 @@ typedef enum CBLChangeTrackerMode {
     id _lastSequenceID;
     unsigned _limit;
     NSError* _error;
+    BOOL _continuous;
     BOOL _includeConflicts;
     NSString* _filterName;
     NSDictionary* _filterParameters;
@@ -65,6 +66,7 @@ typedef enum CBLChangeTrackerMode {
 @property (readonly, nonatomic) NSString* databaseName;
 @property (readonly) NSURL* changesFeedURL;
 @property (readonly, copy, nonatomic) id lastSequenceID;
+@property (nonatomic) BOOL continuous;  // If true, never give up due to errors
 @property (strong, nonatomic) NSError* error;
 @property (weak, nonatomic) id<CBLChangeTrackerClient> client;
 @property (strong, nonatomic) NSDictionary *requestHeaders;

Source/ChangeTracker/CBLChangeTracker.m
@@ -25,7 +25,7 @@
 #define kDefaultHeartbeat (5 * 60.0)
 
 #define kInitialRetryDelay 2.0      // Initial retry delay (doubles after every subsequent failure)
-#define kMaxRetryDelay 300.0        // ...but will never get longer than this
+#define kMaxRetryDelay (10*60.0)    // ...but will never get longer than this
 
 
 @interface CBLChangeTracker ()
@@ -36,7 +36,7 @@
 @implementation CBLChangeTracker
 
 @synthesize lastSequenceID=_lastSequenceID, databaseURL=_databaseURL, mode=_mode;
-@synthesize limit=_limit, heartbeat=_heartbeat, error=_error;
+@synthesize limit=_limit, heartbeat=_heartbeat, error=_error, continuous=_continuous;
 @synthesize client=_client, filterName=_filterName, filterParameters=_filterParameters;
 @synthesize requestHeaders = _requestHeaders, authorizer=_authorizer;
 @synthesize docIDs = _docIDs;
@@ -167,17 +167,17 @@
 
 - (void) failedWithError: (NSError*)error {
     // If the error may be transient (flaky network, server glitch), retry:
-    if (CBLMayBeTransientError(error)) {
+    if (!CBLIsPermanentError(error) && (_continuous || CBLMayBeTransientError(error))) {
         NSTimeInterval retryDelay = kInitialRetryDelay * (1 << MIN(_retryCount, 16U));
         retryDelay = MIN(retryDelay, kMaxRetryDelay);
         ++_retryCount;
-        Log(@"%@: Connection error, retrying in %.1f sec: %@",
-            self, retryDelay, error.localizedDescription);
+        Log(@"%@: Connection error #%d, retrying in %.1f sec: %@",
+            self, _retryCount, retryDelay, error.localizedDescription);
         [self performSelector: @selector(retry) withObject: nil afterDelay: retryDelay];
     } else {
         Warn(@"%@: Can't connect, giving up: %@", self, error);
         self.error = error;
-        [self stopped];
+        [self stop];
     }
 }
 

commit 90542944d15aa4868811d46ff91c04982413589e

Fixed failure in basic-auth of change tracker
Regression introduced by commit e4c2115. Fixes #121.

Source/ChangeTracker/CBLSocketChangeTracker.m
@@ -253,7 +253,8 @@
             if (_credential) {
                 // Recoverable auth failure -- close socket but try again with _credential:
                 _unauthResponse = response;
-                [self failedWithError: CBLStatusToNSError((CBLStatus)status, self.changesFeedURL)];
+                [self clearConnection];
+                [self retry];
                 return NO;
             }
         }