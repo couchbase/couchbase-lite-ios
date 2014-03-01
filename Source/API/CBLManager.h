64f2047c75e60c4cda741211740d3186d5be391d
Jens Alfke <jens@couchbase.com>
2014-02-28 17:02:57 -0800

    Fixed target OS requirements
    * Some iOS targets were still set to iOS 5; updated everything to iOS 6.
    * Updated Mac OS target to OS X 10.8.
    * Added a (strong) annotation to a dispatch_queue_t property in CBLManager.h
    to avoid a warning with OS X 10.8+.
    * Updated ErrorUtils in MYUtilities to stop using a deprecated function.

--- Source/API/CBLManager.h
+++ Source/API/CBLManager.h
@@ -90,7 +90,7 @@ typedef struct CBLManagerOptions {
     manager was instantiated. By setting a dispatch queue, you can call the objects from within that
     queue no matter what the underlying thread is, and notifications will be posted on that queue
     as well. */
-@property dispatch_queue_t dispatchQueue;
+@property (strong) dispatch_queue_t dispatchQueue;
 
 /** Runs the block asynchronously on the database manager's dispatch queue or thread.
     Unlike the rest of the API, this can be called from any thread, and provides a limited form
