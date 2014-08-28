aa2bec070161117cd021c44b6d6c02dbcec1e4c2
Jens Alfke <jens@couchbase.com>
2014-08-28 13:26:21 -0700

    Wrapped long comment lines in CBLQuery.h

--- Source/API/CBLQuery.h
+++ Source/API/CBLQuery.h
@@ -65,10 +65,17 @@ typedef NS_ENUM(unsigned, CBLIndexUpdateMode) {
 @property BOOL inclusiveEnd;
 
 /** If nonzero, enables prefix matching of string or array keys.
-    * A value of 1 treats the endKey itself as a prefix: if it's a string, keys in the index that come after the endKey, but begin with the same prefix, will be matched. (For example, if the endKey is "foo" then the key "foolish" in the index will be matched, but not "fong".) Or if the endKey is an array, any array beginning with those elements will be matched. (For example, if the endKey is [1], then [1, "x"] will match, but not [2].) If the key is any other type, there is no effect.
-    * A value of 2 assumes the endKey is an array and treats its final item as a prefix, using the rules above. (For example, an endKey of [1, "x"] will match [1, "xtc"] but not [1, "y"].)
+    * A value of 1 treats the endKey itself as a prefix: if it's a string, keys in the index that
+      come after the endKey, but begin with the same prefix, will be matched. (For example, if the
+      endKey is "foo" then the key "foolish" in the index will be matched, but not "fong".) Or if
+      the endKey is an array, any array beginning with those elements will be matched. (For
+      example, if the endKey is [1], then [1, "x"] will match, but not [2].) If the key is any
+      other type, there is no effect.
+    * A value of 2 assumes the endKey is an array and treats its final item as a prefix, using the
+      rules above. (For example, an endKey of [1, "x"] will match [1, "xtc"] but not [1, "y"].)
     * A value of 3 assumes the key is an array of arrays, etc.
-    Note that if the .descending property is also set, the search order is reversed and the above discussion applies to the startKey, _not_ the endKey. */
+    Note that if the .descending property is also set, the search order is reversed and the above
+    discussion applies to the startKey, _not_ the endKey. */
 @property NSUInteger prefixMatchLevel;
 
 /** An optional array of NSSortDescriptor objects; overrides the default by-key ordering.
@@ -132,8 +139,8 @@ typedef NS_ENUM(unsigned, CBLIndexUpdateMode) {
 @end
 
 
-/** A CBLQuery subclass that automatically refreshes the result rows every time the database changes.
-    All you need to do is use KVO to observe changes to the .rows property. */
+/** A CBLQuery subclass that automatically refreshes the result rows every time the database
+    changes. All you need to do is use KVO to observe changes to the .rows property. */
 @interface CBLLiveQuery : CBLQuery
 
 /** Starts observing database changes. The .rows property will now update automatically. (You 
@@ -203,16 +210,21 @@ typedef NS_ENUM(unsigned, CBLIndexUpdateMode) {
 /** The row's key: this is the first parameter passed to the emit() call that generated the row. */
 @property (readonly) id key;
 
-/** The row's value: this is the second parameter passed to the emit() call that generated the row. */
+/** The row's value: this is the second parameter passed to the emit() call that generated the
+    row. */
 @property (readonly) id value;
 
 /** The ID of the document described by this view row.
-    This is not necessarily the same as the document that caused this row to be emitted; see the discussion of the .sourceDocumentID property for details. */
+    This is not necessarily the same as the document that caused this row to be emitted; see the
+    discussion of the .sourceDocumentID property for details. */
 @property (readonly) NSString* documentID;
 
 /** The ID of the document that caused this view row to be emitted.
     This is the value of the "id" property of the JSON view row.
-    It will be the same as the .documentID property, unless the map function caused a related document to be linked by adding an "_id" key to the emitted value; in this case .documentID will refer to the linked document, while sourceDocumentID always refers to the original document.
+    It will be the same as the .documentID property, unless the map function caused a related
+    document to be linked by adding an "_id" key to the emitted value; in this case .documentID
+    will refer to the linked document, while sourceDocumentID always refers to the original 
+    document.
     In a reduced or grouped query the value will be nil, since the rows don't correspond to
     individual documents. */
 @property (readonly) NSString* sourceDocumentID;
@@ -223,7 +235,8 @@ typedef NS_ENUM(unsigned, CBLIndexUpdateMode) {
 @property (readonly) CBLDatabase* database;
 
 /** The document this row was mapped from.
-    This will be nil if a grouping was enabled in the query, because then the result rows don't correspond to individual documents. */
+    This will be nil if a grouping was enabled in the query, because then the result rows don't
+    correspond to individual documents. */
 @property (readonly) CBLDocument* document;
 
 /** The properties of the document this row was mapped from.
