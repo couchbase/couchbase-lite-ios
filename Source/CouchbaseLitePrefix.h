c250fba59e7a9d8e6c657fbb62386d5e094882bc
Jens Alfke <jens@couchbase.com>
2014-08-29 11:09:57 -0700

    Rename Obj-C classes from 3rd party libraries, like DDLog & HTTPServer
    Since Obj-C has a global class name table at runtime, there will be
    trouble if a process ever contains two classes with the same name (even
    though we're careful not to export symbols we don't need to.) This has
    led to trouble in apps that link both with CBL and with libraries we
    use such as CocoaLumberjack and CocoaHTTPServer.
    To fix this, I've added some #defines in the prefix header that rename
    all the 3rd party classes  to have a "CBL_" prefix.

--- Source/CouchbaseLitePrefix.h
+++ Source/CouchbaseLitePrefix.h
@@ -26,3 +26,44 @@
 // Configuration for the sqlite3-unicodesn library:
 #define SQLITE_ENABLE_FTS4 
 #define SQLITE_ENABLE_FTS4_UNICODE61
+
+
+// Rename the MYUtilities classes to avoid collisions:
+#define MYDynamicObject             CBL_MYDynamicObject
+
+// Rename the GCDAsyncSocket classes to avoid collisions:
+#define GCDAsyncSocket              CBL_GCDAsyncSocket
+#define GCDAsyncSocketPreBuffer     CBL_GCDAsyncSocketPreBuffer
+#define GCDAsyncReadPacket          CBL_GCDAsyncReadPacket
+#define GCDAsyncWritePacket         CBL_GCDAsyncWritePacket
+#define GCDAsyncSpecialPacket       CBL_GCDAsyncSpecialPacket
+
+// Rename the CocoaLumberjack classes to avoid collisions:
+#define DDLog                       CBL_DDLog
+#define DDLogMessage                CBL_DDLogMessage
+#define DDAbstractLogger            CBL_DDAbstractLogger
+#define DDLoggerNode                CBL_DDLoggerNode
+
+// Rename the CocoaHTTPServer classes to avoid collisions:
+#define HTTPAsyncFileResponse       CBL_HTTPAsyncFileResponse
+#define HTTPAuthenticationRequest   CBL_HTTPAuthenticationRequest
+#define HTTPConfig                  CBL_HTTPConfig
+#define HTTPConnection              CBL_HTTPConnection
+#define HTTPDataResponse            CBL_HTTPDataResponse
+#define HTTPDynamicFileResponse     CBL_HTTPDynamicFileResponse
+#define HTTPFileResponse            CBL_HTTPFileResponse
+#define HTTPMessage                 CBL_HTTPMessage
+#define HTTPRedirectResponse        CBL_HTTPRedirectResponse
+#define HTTPServer                  CBL_HTTPServer
+
+// Rename the oathconsumer classes to avoid collisions:
+#define OAConsumer                  CBL_OAConsumer
+#define OAMutableURLRequest         CBL_OAMutableURLRequest
+#define OAPlaintextSignatureProvider  CBL_OAPlaintextSignatureProvider
+#define OARequestParameter          CBL_OARequestParameter
+#define OAToken                     CBL_OAToken
+
+// Rename the WebScket classes to avoid collisions:
+#define WebSocket                   CBL_WebSocket
+#define WebSocketClient             CBL_WebSocketClient
+#define WebSocketHTTPLogic          CBL_WebSocketHTTPLogic
