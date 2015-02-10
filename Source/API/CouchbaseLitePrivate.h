couchbase/couchbase-lite-ios

commit 56ef7a202480885ad33a27e299b667fbcdb86f9a

Allow a doc to be updated more than once in a single transaction
This didn't use to work correctly because the CBLDocument's currentRevision didn't
update to the new revision after the first update; then the second update would use
an obsolete _rev and fail with a conflict.
Added code to inform the CBLDocument immediately, even during a transaction, but
prevent the change notification from being posted till the transaction ends.
Fixes #256

Source/API/CouchbaseLitePrivate.h
@@ -70,7 +70,9 @@
 - (instancetype) initWithDatabase: (CBLDatabase*)database
                        documentID: (NSString*)docID                 __attribute__((nonnull));
 - (CBLSavedRevision*) revisionFromRev: (CBL_Revision*)rev;
-- (void) revisionAdded: (CBLDatabaseChange*)change                 __attribute__((nonnull));
+- (void) revisionAdded: (CBLDatabaseChange*)change
+                notify: (BOOL)notify                                __attribute__((nonnull));
+- (void) forgetCurrentRevision;
 - (void) loadCurrentRevisionFrom: (CBLQueryRow*)row                 __attribute__((nonnull));
 - (CBLSavedRevision*) putProperties: (NSDictionary*)properties
                      prevRevID: (NSString*)prevID

Source/API/CBLDatabase.m
@@ -109,7 +109,7 @@ static id<CBLFilterCompiler> sFilterCompiler;
     BOOL external = NO;
     for (CBLDatabaseChange* change in changes) {
         // Notify the corresponding instantiated CBLDocument object (if any):
-        [[self _cachedDocumentWithID: change.documentID] revisionAdded: change];
+        [[self _cachedDocumentWithID: change.documentID] revisionAdded: change notify: YES];
         if (change.source != nil)
             external = YES;
     }

Source/API/CBLDocument.m
@@ -158,7 +158,7 @@ NSString* const kCBLDocumentChangeNotification = @"CBLDocumentChange";
 
 
 // Notification from the CBLDatabase that a (current, winning) revision has been added
-- (void) revisionAdded: (CBLDatabaseChange*)change {
+- (void) revisionAdded: (CBLDatabaseChange*)change notify: (BOOL)notify {
     CBL_Revision* rev = change.winningRevision;
     if (!rev)
         return; // current revision didn't change
@@ -172,10 +172,12 @@ NSString* const kCBLDocumentChangeNotification = @"CBLDocumentChange";
     id<CBLDocumentModel> model = _modelObject; // strong reference to it
     [model document: self didChange: change];
 
-    NSNotification* n = [NSNotification notificationWithName: kCBLDocumentChangeNotification
-                                                      object: self
-                                                    userInfo: @{@"change": change}];
-    [[NSNotificationCenter defaultCenter] postNotification: n];
+    if (notify) {
+        NSNotification* n = [NSNotification notificationWithName: kCBLDocumentChangeNotification
+                                                          object: self
+                                                        userInfo: @{@"change": change}];
+        [[NSNotificationCenter defaultCenter] postNotification: n];
+    }
 }
 
 

Source/CBLDatabase+Internal.m
@@ -276,7 +276,12 @@ static BOOL sAutoCompact = YES;
     if (!_changesToNotify)
         _changesToNotify = [[NSMutableArray alloc] init];
     [_changesToNotify addObject: change];
-    [self postChangeNotifications];
+    if (![self postChangeNotifications]) {
+        // The notification wasn't posted yet, probably because a transaction is open.
+        // But the CBLDocument, if any, needs to know right away so it can update its
+        // currentRevision.
+        [[self _cachedDocumentWithID: change.documentID] revisionAdded: change notify: NO];
+    }
 }
 
 /** Posts a local NSNotification of multiple new revisions. */
@@ -288,7 +293,8 @@ static BOOL sAutoCompact = YES;
 }
 
 
-- (void) postChangeNotifications {
+- (BOOL) postChangeNotifications {
+    BOOL posted = NO;
     // This is a 'while' instead of an 'if' because when we finish posting notifications, there
     // might be new ones that have arrived as a result of notification handlers making document
     // changes of their own (the replicator manager will do this.) So we need to check again.
@@ -318,15 +324,21 @@ static BOOL sAutoCompact = YES;
                                                             object: self
                                                           userInfo: $dict({@"changes", changes})];
 
+        posted = YES;
         _postingChangeNotifications = false;
     }
+    return posted;
 }
 
 
 // CBL_StorageDelegate method
 - (void) storageExitedTransaction: (BOOL)committed {
-    if (!committed)
+    if (!committed) {
+        // I already told cached CBLDocuments about these new revisions. Back that out:
+        for (CBLDatabaseChange* change in _changesToNotify)
+            [[self _cachedDocumentWithID: change.documentID] forgetCurrentRevision];
         _changesToNotify = nil;
+    }
     [self postChangeNotifications];
 }
 

Unit-Tests/Database_Tests.m
@@ -308,6 +308,66 @@
 }
 
 
+- (void) test075_UpdateDocInTransaction {
+    // Test for #256, "Conflict error when updating a document multiple times in transaction block"
+    CBLDocument* doc = [self createDocumentWithProperties: @{@"testNumber": @7.5,
+                                                             @"count": @1}];
+
+    __block BOOL notified = NO;
+    id observer = [[NSNotificationCenter defaultCenter]
+                                            addObserverForName: kCBLDocumentChangeNotification
+                                                        object: doc
+                                                         queue: nil
+                                                    usingBlock: ^(NSNotification *note) {
+                                                        notified = YES;
+                                                    }];
+    __block CBLRevision* rev3;
+    [db inTransaction: ^BOOL {
+        // Update doc. The currentRevision should update, but no notification be posted (yet).
+        NSError* error;
+        NSMutableDictionary* props = [doc.properties mutableCopy];
+        props[@"count"] = @2;
+        CBLRevision* rev2 = [doc putProperties: props error: &error];
+        Assert(rev2 != nil, @"1st update failed: %@", error);
+        AssertEqual(doc.currentRevision, rev2);
+        Assert(!notified);
+
+        // Update doc again; this should succeed, in the same manner.
+        props = [doc.properties mutableCopy];
+        props[@"count"] = @3;
+        rev3 = [doc putProperties: props error: &error];
+        Assert(rev3 != nil, @"2nd update failed: %@", error);
+        AssertEqual(doc.currentRevision, rev3);
+        Assert(!notified);
+        return YES;
+    }];
+    AssertEqual(doc.currentRevision, rev3);
+    // Notifications should be posted as soon as the transaction exits:
+    Assert(notified);
+
+    // Now get sneaky and try a failed transaction -- the doc rev change should be backed out:
+    notified = NO;
+    [db inTransaction: ^BOOL {
+        // Update doc. The currentRevision should update, but no notification be posted (yet).
+        NSError* error;
+        NSMutableDictionary* props = [doc.properties mutableCopy];
+        props[@"count"] = @4;
+        CBLRevision* rev4 = [doc putProperties: props error: &error];
+        Assert(rev4 != nil, @"3rd update failed: %@", error);
+        AssertEqual(doc.currentRevision, rev4);
+        Assert(!notified);
+        return NO;
+    }];
+    // No notification should be posted by the aborted transaction:
+    Assert(!notified);
+    // The doc should know that it's back to revision 3:
+    AssertEqual(doc[@"count"], @3);
+    AssertEqual(doc.currentRevision, rev3);
+
+    [[NSNotificationCenter defaultCenter] removeObserver: observer];
+}
+
+
 - (void) test08_SaveDocumentWithNaNProperty {
     NSDictionary* properties = @{@"aNumber": [NSDecimalNumber notANumber]};
     CBLDocument* doc = [db createDocument];

commit 2d7ce70ad37cb052467c72ff6e0e3cfb2f1caf42

Fixed compile error in Release build (regression from 56ef7a2)

Source/API/CouchbaseLitePrivate.h
@@ -40,9 +40,7 @@
 - (void) addReplication: (CBLReplication*)repl;
 - (void) forgetReplication: (CBLReplication*)repl;
 - (void) _clearDocumentCache;
-#if DEBUG // for testing
 - (CBLDocument*) _cachedDocumentWithID: (NSString*)docID;
-#endif
 @end
 
 @interface CBLDatabase (Private)