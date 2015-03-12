0a681b10fe0b8a7408eef6bf045c365d953b8ddc
Jens Alfke <jens@couchbase.com>
2015-03-12 11:18:48 -0700

    Added (dylib-based) iOS framework target
    The "CBL iOS Framework" target builds a true dynamic-library framework
    for use in iOS 8.
    Note: This library isn't fat. I'm actually unsure whether we should
    build a fat dynamic library; I don't know if the simulator code will get
    stripped out when the developer's app is built.
    
    Changed the way the static library is built, slightly:
    * The "CBL iOS Library" target builds a static library, but it's no
      longer fat.
    * A new "CBL iOS fat library" target simply creates a fat version of the
      above static library.
    Fixes #597

---
+++ Source/noop.m
@@ -0,0 +1,13 @@
+//
+//  noop.m
+//  CouchbaseLite
+//
+//  Created by Jens Alfke on 3/12/15.
+//  Copyright (c) 2015 Couchbase, Inc. All rights reserved.
+//
+
+void noop(void);
+
+void noop() {
+
+}
