d2a91262e93377f5dea57777b92a7c152665577f
Jens Alfke <jens@couchbase.com>
2015-03-25 16:03:16 -0700

    Added "CouchbaseLiteListener.h" header to resolve new Xcode warning
    When building framework as a module, there needs to be an "umbrella
    header" with the same name as the module.

---
+++ Listener/CouchbaseLiteListener.h
@@ -0,0 +1,9 @@
+//
+//  CouchbaseLiteListener.h
+//  CouchbaseLite
+//
+//  Created by Jens Alfke on 3/25/15.
+//  Copyright (c) 2015 Couchbase, Inc. All rights reserved.
+//
+
+#import "CBLListener.h"
