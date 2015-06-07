028a5d7e29076df22a5102b8b5b64d72b5629e99
Jens Alfke <jens@couchbase.com>
2015-06-07 11:16:26 -0700

    WIP: Adding Bonjour to CBLSyncListener (unfinished)

--- Source/CBLSyncListener.m
+++ Source/CBLSyncListener.m
@@ -16,6 +16,10 @@
 {
     CBLDatabase* _db;
     NSMutableSet* _handlers;
+    dispatch_queue_t _queue;
+    NSString* _bonjourName, *_bonjourType;
+    NSNetService* _netService;
+    BOOL _netServicePublished;
 }
 
 - (instancetype) initWithDatabase: (CBLDatabase*)db
@@ -25,25 +29,30 @@
     if (self) {
         _db = db;
         _handlers = [NSMutableSet new];
+        _queue = dispatch_queue_create("CBLSyncListener", DISPATCH_QUEUE_SERIAL);
     }
     return self;
 }
 
 - (void) blipConnectionDidOpen:(BLIPConnection *)connection {
-    LogTo(Sync, @"OPENED INCOMING %@ from <%@>", connection, connection.URL);
-    NSString* name = $sprintf(@"Sync from %@", connection.URL);
-    dispatch_queue_t queue = dispatch_queue_create(name.UTF8String, DISPATCH_QUEUE_SERIAL);
-    CBLSyncConnection* handler = [[CBLSyncConnection alloc] initWithDatabase: _db
-                                                            connection: connection
-                                                                 queue: queue];
-    [_handlers addObject: handler];
-    [handler addObserver: self forKeyPath: @"state" options: 0 context: (void*)1];
+    dispatch_async(_queue, ^{
+        LogTo(Sync, @"OPENED INCOMING %@ from <%@>", connection, connection.URL);
+        NSString* name = $sprintf(@"Sync from %@", connection.URL);
+        dispatch_queue_t queue = dispatch_queue_create(name.UTF8String, DISPATCH_QUEUE_SERIAL);
+        CBLSyncConnection* handler = [[CBLSyncConnection alloc] initWithDatabase: _db
+                                                                connection: connection
+                                                                     queue: queue];
+        [_handlers addObject: handler];
+        [handler addObserver: self forKeyPath: @"state" options: 0 context: (void*)1];
+    });
 }
 
 - (void) forgetHandler: (CBLSyncConnection*)handler {
-    LogTo(Sync, @"CLOSED INCOMING connection from <%@>", handler.peerURL);
-    [handler removeObserver: self forKeyPath: @"state"];
-    [_handlers removeObject: handler];
+    dispatch_async(_queue, ^{
+        LogTo(Sync, @"CLOSED INCOMING connection from <%@>", handler.peerURL);
+        [handler removeObserver: self forKeyPath: @"state"];
+        [_handlers removeObject: handler];
+    });
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
@@ -56,6 +65,55 @@
     } else {
         [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
     }
+}
+
+#pragma mark - BONJOUR:
+
+- (void) setBonjourName: (NSString*)name type: (NSString*)type {
+    dispatch_async(_queue, ^{
+        _bonjourName = name;
+        _bonjourType = type;
+    });
+}
+
+- (NSString*) bonjourName {
+    return _netServicePublished ? _netService.name : nil;
+}
+
+- (void) listenerDidStart {
+    dispatch_async(_queue, ^{
+        if (_bonjourType) {
+            _netService = [[NSNetService alloc] initWithDomain: @""
+                                                          type: _bonjourType
+                                                          name: _bonjourName
+                                                          port: self.port];
+            _netService.includesPeerToPeer = YES;
+            _netService.delegate = self;
+            _netServicePublished = NO;
+            [_netService scheduleInRunLoop: [NSRunLoop mainRunLoop] forMode: NSDefaultRunLoopMode];
+            [_netService publishWithOptions: 0];
+        }
+    }
+}
+
+- (void) listenerDidStop {
+    dispatch_async(_queue, ^{
+        [_netService stop];
+        _netService = nil;
+        _netServicePublished = NO;
+    }
+}
+
+
+- (void)netServiceDidPublish:(NSNetService *)sender {
+    dispatch_async(_queue, ^{
+        Log(@"CBLSyncListener: Published Bonjour service '%@'", self.name);
+        _netServicePublished = YES;
+    }
+}
+
+- (void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict {
+    Warn(@"CBLSyncListener: Failed to publish Bonjour service '%@': %@", self.name, errorDict);
 }
 
 @end
