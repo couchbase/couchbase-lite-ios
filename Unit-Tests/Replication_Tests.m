f2a7e11b2f85fa0b4f0c1c29af2943e02d64b4ba
Pasin Suriyentrakorn <pasin@couchbase.com>
2015-06-11 10:15:49 +0700

    Fix CBLReplication stop notification timeout warning
    - Calling the -stop method twice or more will cause the 'Timeout waiting for background stop notification' to show up as the _bg_replicator has already been stop so there will not be any more notification sent. To fix the issue, ignore the -stop message if the _bg_replicator is not running.
    
    - Fix comments in test16_Restart and rename test12_StopIdlePush to test12_StopIdlePushReplication for naming consistency.
    
    #758

--- Unit-Tests/Replication_Tests.m
+++ Unit-Tests/Replication_Tests.m
@@ -694,7 +694,7 @@ static UInt8 sEncryptionIV[kCCBlockSizeAES128];
     Assert([importDb deleteDatabase:&error], @"Couldn't delete db: %@", error);
 }
 
-- (void) test12_StopIdlePush {
+- (void) test12_StopIdlePushReplication {
     NSURL* remoteDbURL = [self remoteTestDBURL: kPushThenPullDBName];
     if (!remoteDbURL)
         return;
@@ -919,7 +919,7 @@ static UInt8 sEncryptionIV[kCCBlockSizeAES128];
     [pusher start];
     [pusher restart];
 
-    // Wait to get a notification after the replication is stopped:
+    // Wait to get a notification when the replication is idle:
     NSDate* timeout = [NSDate dateWithTimeIntervalSinceNow: 2.0];
     while (pusher.status != kCBLReplicationIdle && timeout.timeIntervalSinceNow > 0.0) {
         if (![[NSRunLoop currentRunLoop] runMode: NSDefaultRunLoopMode
@@ -948,7 +948,7 @@ static UInt8 sEncryptionIV[kCCBlockSizeAES128];
     [puller start];
     [puller restart];
 
-    // Wait to get a notification after the replication is stopped:
+    // Wait to get a notification when the replication is idle:
     timeout = [NSDate dateWithTimeIntervalSinceNow: 2.0];
     while (puller.status != kCBLReplicationIdle && timeout.timeIntervalSinceNow > 0.0) {
         if (![[NSRunLoop currentRunLoop] runMode: NSDefaultRunLoopMode
