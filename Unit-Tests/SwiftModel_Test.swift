280f22c733f56ddb03c244b7ace57617bc2b4d7c
Jens Alfke <jens@couchbase.com>
2015-06-19 15:54:42 -0700

    Fixed CBLModel not working with namespaced Swift classes
    It's an Apple bug in the Obj-C runtime, but I added a workaround in
    MYDynamicObject.
    Fixes #703

---
+++ Unit-Tests/SwiftModel_Test.swift
@@ -0,0 +1,38 @@
+//
+//  SwiftModel_Test.swift
+//  CouchbaseLite
+//
+//  Created by Jens Alfke on 6/19/15.
+//  Copyright (c) 2015 Couchbase, Inc. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+
+class SwiftModel : CBLModel {
+    @NSManaged var intsy: Int
+    @NSManaged var stringsy: String
+}
+
+
+class SwiftModelWithRelation : CBLModel {
+    @NSManaged var moddy: SwiftModel
+}
+
+
+class SwiftModel_Test: CBLTestCaseWithDB {
+
+    func testSwiftModel() {
+        var model = SwiftModel(forNewDocumentInDatabase: db);
+        model.intsy = 42;
+        model.stringsy = "Frood";
+    }
+
+    func testRelation() {
+        var model = SwiftModel(forNewDocumentInDatabase: db);
+        var relator = SwiftModelWithRelation(forNewDocumentInDatabase: db)
+        relator.moddy = model
+    }
+
+}
