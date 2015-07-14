a8fb03dbea7533b11cfa18df7456153c67a4644b
Jens Alfke <jens@couchbase.com>
2015-07-13 21:56:17 -0700

    Fixed sending mismatched MIME multipart boundary strings on HTTP retry
    If a CBLMultipartWriter created by CBLRestPusher needed to retry the
    request (on auth challenge or redirect), it would get a new body stream,
    but the new stream would have a different random multipart boundary
    string. Unfortunately the old boundary was still encoded in the
    Content-Type header. The resulting HTTP body would be unparseable :(
    
    Fixed by making sure to create the new body with the same boundary.

--- Source/CBLRestPusher.m
+++ Source/CBLRestPusher.m
@@ -405,7 +405,9 @@ CBLStatus CBLStatusFromBulkDocsResponseItem(NSDictionary* item) {
         return kCBLStatusUpstreamError;
 }
 
-- (CBLMultipartWriter*)multipartWriterForRevision: (CBL_Revision*)rev {
+- (CBLMultipartWriter*)multipartWriterForRevision: (CBL_Revision*)rev
+                                         boundary: (NSString*)boundary
+{
     // Find all the attachments with "follows" instead of a body, and put 'em in a multipart stream.
     // It's important to scan the _attachments entries in the same order in which they will appear
     // in the JSON, because CouchDB expects the MIME bodies to appear in that same order (see #133).
@@ -417,7 +419,7 @@ CBLStatus CBLStatusFromBulkDocsResponseItem(NSDictionary* item) {
             if (!bodyStream) {
                 // Create the HTTP multipart stream:
                 bodyStream = [[CBLMultipartWriter alloc] initWithContentType: @"multipart/related"
-                                                                    boundary: nil];
+                                                                    boundary: boundary];
                 [bodyStream setNextPartsHeaders: @{@"Content-Type": @"application/json"}];
                 // Use canonical JSON encoder so that _attachments keys will be written in the
                 // same order that this for loop is processing the attachments.
@@ -456,9 +458,10 @@ CBLStatus CBLStatusFromBulkDocsResponseItem(NSDictionary* item) {
 
 - (BOOL) uploadMultipartRevision: (CBL_Revision*)rev {
     // Pre-creating the body stream and check if it's available or not.
-    __block CBLMultipartWriter* bodyStream = [self multipartWriterForRevision: rev];
+    __block CBLMultipartWriter* bodyStream = [self multipartWriterForRevision: rev boundary: nil];
     if (!bodyStream)
         return NO;
+    NSString* boundary = bodyStream.boundary;
     
     // OK, we are going to upload this on its own:
     self.changesTotal++;
@@ -471,10 +474,13 @@ CBLStatus CBLStatusFromBulkDocsResponseItem(NSDictionary* item) {
                               multipartWriter:^CBLMultipartWriter *{
                                   CBLMultipartWriter* writer = bodyStream;
                                   // Reset to nil so the writer will get regenerated if the block
-                                  // gets re-called (e.g. when retrying).
+                                  // gets re-called (e.g. when retrying). Make sure to use the same
+                                  // multipart boundary string: it's already been encoded in the
+                                  // Content-Type header so it has to match in the body!
                                   bodyStream = nil;
                                   if (!writer)
-                                      writer = [self multipartWriterForRevision: rev];
+                                      writer = [self multipartWriterForRevision: rev
+                                                                       boundary: boundary];
                                   return writer;
                               }
                                  onCompletion: ^(CBLMultipartUploader* result, NSError *error) {
