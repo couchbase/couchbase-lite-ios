couchbase/couchbase-lite-ios

commit a574c15b2184cd1f51cf1c51e58c4fd42fdd3f06

Make CBLView.totalRows public
- Moved totalRows property from CBLView+Internal.h to CBLView.h

- When accessing the totalRows, update the index first.

#808

Source/API/CBLView.m
@@ -324,6 +324,7 @@ static id<CBLViewCompiler> sCompiler;
 
 
 - (NSUInteger) totalRows {
+    [self updateIndex];
     return _storage.totalRows;
 }
 

Source/CBLView+Internal.h
@@ -39,8 +39,6 @@ BOOL CBLRowPassesFilter(CBLDatabase* db, CBLQueryRow* row, const CBLQueryOptions
 
 - (void) close;
 
-@property (readonly) NSUInteger totalRows;
-
 /** The map block alredy registered with the view. Unlike the public .mapBlock property, this
     will not look for a design document or compile a function therein. */
 @property (readonly) CBLMapBlock registeredMapBlock;

Source/API/CBLView.h
@@ -105,6 +105,10 @@ FOUNDATION_EXTERN id CBLTextKey(NSString* text);
 /** The last sequence number indexed so far. */
 @property (readonly) SInt64 lastSequenceIndexed;
 
+/** Total number of rows in the view. The view's index will be updated if needed before returning
+    the totalRows value. */
+@property (readonly) NSUInteger totalRows;
+
 /** Deletes the view's persistent index. It will be regenerated on the next query. */
 - (void) deleteIndex;
 

Unit-Tests/View_Tests.m
@@ -1048,4 +1048,28 @@ static NSDictionary* mkGeoRect(double x0, double y0, double x1, double y1) {
 }
 
 
+- (void) test21_TotalRows {
+    CBLView* view = [db viewNamed: @"vu"];
+    Assert(view);
+    [view setMapBlock: MAPBLOCK({
+        emit(doc[@"sequence"], nil);
+    }) version: @"1"];
+    Assert(view.mapBlock != nil);
+    AssertEq(view.totalRows, 0u);
+
+    // Add 20 documents:
+    [self createDocuments: 20];
+    Assert(view.stale);
+    AssertEq(view.totalRows, 20u);
+    Assert(!view.stale);
+
+    // Add another 20 documents, query, and check totalRows:
+    [self createDocuments: 20];
+    CBLQuery* query = [view createQuery];
+    CBLQueryEnumerator* rows = [query run: NULL];
+    AssertEq(rows.count, 40u);
+    AssertEq(view.totalRows, 40u);
+}
+
+
 @end

commit bae82585a3a68e7a7186a083a2e0fa6834f0c636

Fix query design docs with stale=ok (recent regression)
- The regression introduced by a574c15b2184cd1f51cf1c51e58c4fd42fdd3f06 commit as exposing totalRows to public.

- Fixed by adding internal currentTotalRows property which doesn't update the index and making totalRows calling currentTotalRows after updating the index.

- Fixed ViewInternal_Tests.test26_TotalDocs accordingly.

#808

Source/API/CBLView.m
@@ -323,9 +323,14 @@ static id<CBLViewCompiler> sCompiler;
 #pragma mark - QUERYING:
 
 
+- (NSUInteger) currentTotalRows {
+    return _storage.totalRows;
+}
+
+
 - (NSUInteger) totalRows {
     [self updateIndex];
-    return _storage.totalRows;
+    return [self currentTotalRows];
 }
 
 

Source/CBLView+Internal.h
@@ -39,6 +39,9 @@ BOOL CBLRowPassesFilter(CBLDatabase* db, CBLQueryRow* row, const CBLQueryOptions
 
 - (void) close;
 
+/** Current total rows in the view. The index will not be updated when accessing this property. */
+@property (readonly) NSUInteger currentTotalRows;
+
 /** The map block alredy registered with the view. Unlike the public .mapBlock property, this
     will not look for a design document or compile a function therein. */
 @property (readonly) CBLMapBlock registeredMapBlock;

Source/CBL_Router+Handlers.m
@@ -1137,7 +1137,7 @@ static NSArray* parseJSONRevArrayQuery(NSString* queryStr) {
     NSArray* rows = [self queryIteratorAllRows: iterator];
     id updateSeq = options->updateSeq ? @(view.lastSequenceIndexed) : nil;
     _response.bodyObject = $dict({@"rows", rows},
-                                 {@"total_rows", @(view.totalRows)},
+                                 {@"total_rows", @(view.currentTotalRows)},
                                  {@"offset", @(options->skip)},
                                  {@"update_seq", updateSeq});
     return kCBLStatusOK;

Unit-Tests/ViewInternal_Tests.m
@@ -1362,8 +1362,10 @@ static NSArray* rowsToDictsSettingDB(CBLDatabase* db, CBLQueryIteratorBlock iter
     
     // Create a view
     CBLView* view = [self createView];
-    AssertEq(view.totalRows, 0u);
+    AssertEq(view.currentTotalRows, 0u);
     AssertEq([view updateIndex], kCBLStatusOK);
+    Assert(!view.stale);
+    AssertEq(view.currentTotalRows, totalRows);
     AssertEq(view.totalRows, totalRows);
 
     // Create a conflict, won by the new revision:
@@ -1415,7 +1417,8 @@ static NSArray* rowsToDictsSettingDB(CBLDatabase* db, CBLQueryIteratorBlock iter
     
     // Delete the index
     [view deleteIndex];
-    AssertEq(view.totalRows, 0u);
+    Assert(view.stale);
+    AssertEq(view.currentTotalRows, 0u);
 }
 
 