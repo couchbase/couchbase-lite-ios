3219bee9faa6b92148c25761b1b6c8349290ed48
Jens Alfke <jens@couchbase.com>
2015-08-13 12:57:15 -0700

    Upgraded SSL cert for unit tests to use SHA256 (not SHA1)
    iOS 9's App Transport Security doesn't accept SHA1 digests in certs, so
    this caused unit tests to fail.
    
    New cert was created with:
    openssl req -new -x509 -key privkey.pem -sha256 -out cert.pem -days 1095
    (using the same privkey.pem as before.)

--- Unit-Tests/ReplicatorInternal_Tests.m
+++ Unit-Tests/ReplicatorInternal_Tests.m
@@ -337,7 +337,7 @@
     }];
 
     Log(@"Now replicating with correct pinned cert...");
-    NSString* digest = @"c745fbfc03382125271daffc2e715a5b0172d1d8";
+    NSString* digest = @"29adb68f518347bfa2770723642414f89a4d352a";
     id lastSeq = replic8Continuous(db, remoteURL, NO, nil,
                                    @{kCBLReplicatorOption_PinnedCert: digest},
                                    nil);
