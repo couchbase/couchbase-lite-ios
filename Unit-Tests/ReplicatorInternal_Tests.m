284f2807a69694cba5ffe9be1662eb5bc998e5aa
Jens Alfke <jens@couchbase.com>
2015-08-20 16:23:18 -0700

    Stop using hardcoded cert fingerprint in test_08_Puller_SSL_Pinned
    A different cert needs to be substituted when testing on a device
    since the SG will not be on 'localhost', and this cert will have a
    different digest of course. So compute the digest at runtime.
    Fixes #882
    
    # Conflicts:
    #	Unit-Tests/ReplicatorInternal_Tests.m

--- Unit-Tests/ReplicatorInternal_Tests.m
+++ Unit-Tests/ReplicatorInternal_Tests.m
@@ -292,7 +292,7 @@
                                       userInfo: nil]);
 
     Log(@"Now replicating with correct pinned cert...");
-    digest = @"29adb68f518347bfa2770723642414f89a4d352a";
+    digest = CBLHexSHA1Digest([self contentsOfTestFile: @"SelfSigned.cer"]);
     id lastSeq = replic8Continuous(db, remoteURL, NO, nil,
                                    @{kCBLReplicatorOption_PinnedCert: digest},
                                    nil);
