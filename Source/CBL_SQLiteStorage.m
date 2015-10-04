couchbase/couchbase-lite-ios

commit e371f531085f1f978a60c814cbadc738312c705c

After exporting re-keyed encrypted db, set SQLite user_version
Fixes #930

Source/CBL_SQLiteStorage.m
@@ -330,7 +330,10 @@ static void CBLComputeFTSRank(sqlite3_context *pCtx, int nVal, sqlite3_value **a
 
         // Export the current database's contents to the new one:
         [action addPerform:^BOOL(NSError **outError) {
-            return [self checkUpdate: [_fmdb executeUpdate: @"SELECT sqlcipher_export('rekeyed_db')"]
+            return [self checkUpdate: [_fmdb executeUpdate:@"SELECT sqlcipher_export('rekeyed_db')"]
+                               error: outError]
+                && [self checkUpdate: [_fmdb executeUpdate: @"PRAGMA rekeyed_db.user_version = ?",
+                                       @(self.schemaVersion)]
                                error: outError];
         } backOut: NULL cleanUp: NULL];
     }

commit 62256ddaebe9301a783ea7e1a76f02f9566c0d54

Fixed problem updating SQLite user_version
Regression from e371f53.
Fixes #935 (thanks, Brendan)

Source/CBL_SQLiteStorage.m
@@ -330,10 +330,10 @@ static void CBLComputeFTSRank(sqlite3_context *pCtx, int nVal, sqlite3_value **a
 
         // Export the current database's contents to the new one:
         [action addPerform:^BOOL(NSError **outError) {
+            NSString* vers = $sprintf(@"PRAGMA rekeyed_db.user_version = %d", self.schemaVersion);
             return [self checkUpdate: [_fmdb executeUpdate:@"SELECT sqlcipher_export('rekeyed_db')"]
                                error: outError]
-                && [self checkUpdate: [_fmdb executeUpdate: @"PRAGMA rekeyed_db.user_version = ?",
-                                       @(self.schemaVersion)]
+                && [self checkUpdate: [_fmdb executeUpdate: vers]
                                error: outError];
         } backOut: NULL cleanUp: NULL];
     }