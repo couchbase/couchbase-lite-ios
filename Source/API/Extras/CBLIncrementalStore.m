873d5960f503a910abdaf55c3af60ea24601c199
Pasin Suriyentrakorn <pasin@couchbase.com>
2015-10-20 17:01:38 -0700

    CBLIS uses view.lastSequenceChangedAt instead of database.lastSequenceNumber
    When checking if the to-many relationship cache is still valid, checking against `CBLView.lastSequenceChangedAt` instead of CBLDatabase.lastSequenceNumber. Note that `CBLView.lastSequenceChangedAt` is a private API.
    
    #938.

--- Source/API/Extras/CBLIncrementalStore.m
+++ Source/API/Extras/CBLIncrementalStore.m
@@ -56,6 +56,10 @@ static NSError* CBLISError(NSInteger code, NSString* desc, NSError *parent);
 - (NSString*) couchbaseLiteIDRepresentation;
 @end
 
+@interface CBLView (CBLIncrementalStore)
+@property (readonly) SInt64 lastSequenceChangedAt;
+@end
+
 
 @interface CBLIncrementalStore ()
 
@@ -1624,7 +1628,7 @@ static CBLManager* sCBLManager;
     if (view) {
         NSString* cacheKey = [NSString stringWithFormat:@"%@/%@", view, parentKey];
         CBLQueryEnumerator* result = [_relationshipCache objectForKey: cacheKey];
-        if (result && (SInt64)result.sequenceNumber == view.database.lastSequenceNumber)
+        if (result && (SInt64)result.sequenceNumber == view.lastSequenceChangedAt)
             return [result allObjects];
 
         CBLQuery* query = [view createQuery];
