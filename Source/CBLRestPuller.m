188fc8e68222ab614c8750f5aa0f2ecd72c637a7
Jens Alfke <jens@couchbase.com>
2015-11-03 10:44:36 -0800

    Always use the POST form of _changes, since CouchDB supports it
    This avoids the potential of ridiculously long URLs if the replication
    has a huge set of filter parameters, e.g. channel names.
    Fixes #964

--- Source/CBLRestPuller.m
+++ Source/CBLRestPuller.m
@@ -113,7 +113,7 @@
     _changeTracker.docIDs = _settings.docIDs;
     _changeTracker.authorizer = _authorizer;
     _changeTracker.cookieStorage = self.cookieStorage;
-    _changeTracker.usePOST = [self serverIsSyncGatewayVersion: @"0.93"];
+    _changeTracker.usePOST = YES; // both SG and CouchDB support POST to _changes
 
     unsigned heartbeat = $castIf(NSNumber, _settings.options[kCBLReplicatorOption_Heartbeat]).unsignedIntValue;
     if (heartbeat >= 15000)
