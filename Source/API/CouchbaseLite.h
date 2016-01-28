couchbase/couchbase-lite-ios

commit e97746843befc7987db35420bfbcddad435c9968

Remove CBLRegisterJSViewCompiler.h from CouchbaseLite.h
#1064

Source/API/CouchbaseLite.h
@@ -32,7 +32,6 @@
 #import "CBLModel.h"
 #import "CBLModelFactory.h"
 #import "CBLJSON.h"
-#import "CBLRegisterJSViewCompiler.h"
 
 #if TARGET_OS_IPHONE
 #import "CBLUITableSource.h"

commit ec82c9feaedc375d40788a3e1cba79f87edc3424

Include CBLRegisterJSViewCompiler.h for Mac only
This is a regression bug after e97746843befc7987db35420bfbcddad435c9968 commit.

OSX includes CBLRegisterJSViewCompiler into the framework but iOS doesn't so we need to add CBLRegisterJSViewCompiler.h back to CouchbaesLite.h for Mac only.

#1054

Source/API/CouchbaseLite.h
@@ -35,4 +35,6 @@
 
 #if TARGET_OS_IPHONE
 #import "CBLUITableSource.h"
+#else
+#import "CBLRegisterJSViewCompiler.h"
 #endif