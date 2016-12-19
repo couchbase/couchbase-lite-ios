4841d937cb324cd518700e21b5c69ebbda5f0fd9
Jens Alfke <jens@couchbase.com>
2016-12-19 08:55:55 -0800

    Limit size of _bulk_get request to 100 docs
    This is consistent with the C# implementation. Also, apparently some
    deployments have trouble with extremely large responses from SG.
    Fixes #1500

--- Source/CBLRestPuller.m
+++ Source/CBLRestPuller.m
@@ -44,8 +44,8 @@
 // run out, even if the CBL thread doesn't always have time to run.)
 #define kMaxOpenHTTPConnections 12
 
-// Maximum number of revs to fetch in a single _all_docs request (CouchDB only, not SG)
-#define kMaxRevsToGetInBulkWithAllDocs 50u
+// Maximum number of revs to fetch in a single request (_bulk_get, or _all_docs w/CouchDB)
+#define kMaxRevsToGetInBulk 100u
 
 // Maximum number of revs we want to be handling at once -- that's all revs that we've heard about
 // from the change tracker but haven't yet inserted into the database. Once we hit this limit we
@@ -372,9 +372,7 @@
 // Start up some HTTP GETs, within our limit on the maximum simultaneous number
 - (void) pullRemoteRevisions {
     while (_db && _httpConnectionCount < kMaxOpenHTTPConnections) {
-        NSUInteger nBulk = _bulkRevsToPull.count;
-        if (!_canBulkGet)
-            nBulk = MIN(nBulk, kMaxRevsToGetInBulkWithAllDocs);
+        NSUInteger nBulk = MIN(_bulkRevsToPull.count, kMaxRevsToGetInBulk);
         if (nBulk == 1) {
             // Rather than pulling a single revision in 'bulk', just pull it normally:
             [self queueRemoteRevision: _bulkRevsToPull[0]];
