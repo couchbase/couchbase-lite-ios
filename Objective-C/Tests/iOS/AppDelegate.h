afc5e6072af6cc24f01a6d5e6a7e44f5bfe17aeb
Pasin Suriyentrakorn <pasin@couchbase.com>
2017-02-03 00:21:37 -0800

    Add iOS Unit Test App
    * Added CBL iOS Tests App and CBL iOS Tests (Unit Test Bundle) targets
    * Added CBL iOS Tests App.xcconfig and CBL iOS Tests.xcconfig
    * Made CBL iOS Tests App shared scheme

---
+++ Objective-C/Tests/iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CBL Tests
+//
+//  Created by Pasin Suriyentrakorn on 2/2/17.
+//  Copyright © 2017 Couchbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/Tests/iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CBL Tests
+//
+//  Created by Pasin Suriyentrakorn on 2/2/17.
+//  Copyright © 2017 Couchbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
