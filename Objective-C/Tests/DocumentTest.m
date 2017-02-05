aca0bf12693b401dbab92686d055f959c00f26d7
Jens Alfke <jens@couchbase.com>
2017-02-05 10:56:04 -0800

    Fixed a roundoff error in date parsing that broke round-trip fidelity
    NSString -> NSDate -> NSString conversion would sometimes result in a
    different string (last digit of milliseconds changed), due to roundoff
    error in the date parser.
    
    Fixes #1611

--- Objective-C/Tests/DocumentTest.m
+++ Objective-C/Tests/DocumentTest.m
@@ -207,6 +207,18 @@
 }
 
 
+// Verify that round trip NSString -> NSDate -> NSString conversion doesn't alter the string (#1611)
+- (void) testJSONDateRoundTrip {
+    NSString* dateStr1 = @"2017-02-05T18:14:06.347Z";
+    NSDate* date1 = [CBLJSON dateWithJSONObject: dateStr1];
+    NSString* dateStr2 = [CBLJSON JSONObjectWithDate: date1];
+    NSDate* date2 = [CBLJSON dateWithJSONObject: dateStr2];
+    XCTAssertEqualWithAccuracy(date2.timeIntervalSinceReferenceDate,
+                               date1.timeIntervalSinceReferenceDate, 0.0001);
+    AssertEqualObjects(dateStr2, dateStr1);
+}
+
+
 - (void) testPropertyAccessors {
     // Premitives:
     [doc setBoolean: YES forKey: @"bool"];
