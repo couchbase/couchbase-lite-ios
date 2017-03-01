3511741f12b7346a68de5deba39fffa1703a70ed
Pasin Suriyentrakorn <pasin@couchbase.com>
2017-03-01 13:15:27 -0800

    Fixed simulator swift modules not being copied when making FAT framework
    When making Swift FAT framework, also copying all of the Swift modules from all sub frameworks.

--- Scripts/build_framework.sh
+++ Scripts/build_framework.sh
@@ -60,6 +60,14 @@ rm -rf "${OUTPUT_BASE_DIR}"
 ROUND=0
 OUTPUT_BINS=()
 OUTPUT_DSYM=()
+OUTPUT_SWIFT_MODULES=()
+
+# Get binary and framework name:
+BIN_NAME=`xcodebuild -scheme "${SCHEME}" -showBuildSettings|grep -w PRODUCT_NAME|head -n 1|awk '{ print $3 }'`
+FRAMEWORK_FILE_NAME=${BIN_NAME}.framework
+OUTPUT_FRAMEWORK_BUNDLE_DIR=${OUTPUT_BASE_DIR}/${FRAMEWORK_FILE_NAME}
+
+# Building all frameworks based on the SDK list:
 for SDK in "${SDKS[@]}"
   do
     echo "Running xcodebuild on scheme=${SCHEME} and sdk=${SDK} ..."
@@ -69,16 +77,19 @@ for SDK in "${SDKS[@]}"
       CLEAN_CMD="clean "
     fi
 
+    #Run xcodebuild:
     xcodebuild -scheme "${SCHEME}" -configuration Release -sdk ${SDK} OHTER_CFLAGS="-fembed-bitcode" ${CLEAN_CMD}build
-    PRODUCTS_DIR=`xcodebuild -scheme "${SCHEME}" -configuration Release -sdk ${SDK} -showBuildSettings|grep -w BUILT_PRODUCTS_DIR|head -n 1|awk '{ print $3 }'`
-    BIN_NAME=`xcodebuild -scheme "${SCHEME}" -configuration Release -sdk ${SDK} -showBuildSettings|grep -w PRODUCT_NAME|head -n 1|awk '{ print $3 }'`
-    FRAMEWORK_FILE_NAME=${BIN_NAME}.framework
+
+    # Get the XCode built framework and dsym file path:
+    PRODUCTS_DIR=`xcodebuild -scheme "${SCHEME}" -configuration Release -sdk "${SDK}" -showBuildSettings|grep -w BUILT_PRODUCTS_DIR|head -n 1|awk '{ print $3 }'`
     FRAMEWORK_FILE_PATH=${PRODUCTS_DIR}/${FRAMEWORK_FILE_NAME}
     DSYM_FILE_PATH=${FRAMEWORK_FILE_PATH}.dSYM
 
+    # Create output dir to copy the built framework to:
     OUTPUT_SDK_DIR=${OUTPUT_BASE_DIR}/${SDK}
     mkdir -p "${OUTPUT_SDK_DIR}"
 
+    # Copy the framework and dSYM files:
     if [ ${ROUND} == 0 ]
     then
       cp -a "${FRAMEWORK_FILE_PATH}" "${OUTPUT_BASE_DIR}"
@@ -88,21 +99,38 @@ for SDK in "${SDKS[@]}"
     cp -a "${FRAMEWORK_FILE_PATH}" "${OUTPUT_SDK_DIR}"
     cp -a "${DSYM_FILE_PATH}" "${OUTPUT_SDK_DIR}"
 
+    # Collect output paths to use for making the FAT framework:
     OUTPUT_BINS+=("\"${OUTPUT_SDK_DIR}/${FRAMEWORK_FILE_NAME}/${BIN_NAME}\"")
     OUTPUT_DSYM+=("\"${OUTPUT_SDK_DIR}/${FRAMEWORK_FILE_NAME}.dSYM/Contents/Resources/DWARF/${BIN_NAME}\"")
+    SWIFT_MODULE_DIR=${OUTPUT_SDK_DIR}/${FRAMEWORK_FILE_NAME}/Modules/${BIN_NAME}.swiftmodule
+    if [ -d "${SWIFT_MODULE_DIR}" ]
+    then
+      OUTPUT_SWIFT_MODULES+=("${SWIFT_MODULE_DIR}")
+    fi
+
     ROUND=$((ROUND + 1))
 done
 
+# Make FAT framework:
 if [[ ${#SDKS[@]} > 1 ]]
 then
+  # Binary:
   LIPO_BIN_INPUTS=$(IFS=" " ; echo "${OUTPUT_BINS[*]}")
   echo "Generate FAT binary: ${LIPO_BIN_INPUTS}"
-  LIPO_CMD="lipo ${LIPO_BIN_INPUTS} -create -output \"${OUTPUT_BASE_DIR}/${FRAMEWORK_FILE_NAME}/${BIN_NAME}\""
+  LIPO_CMD="lipo ${LIPO_BIN_INPUTS} -create -output \"${OUTPUT_FRAMEWORK_BUNDLE_DIR}/${BIN_NAME}\""
   eval "${LIPO_CMD}"
 
+  # dSYM file:
   LIPO_DSYM_INPUTS=$(IFS=" " ; echo "${OUTPUT_DSYM[*]}")
   echo "Generate FAT dSYM: ${LIPO_DSYM_INPUTS}"
-  LIPO_CMD="lipo ${LIPO_DSYM_INPUTS} -create -output \"${OUTPUT_BASE_DIR}/${FRAMEWORK_FILE_NAME}.dSYM/Contents/Resources/DWARF/${BIN_NAME}\""
+  LIPO_CMD="lipo ${LIPO_DSYM_INPUTS} -create -output \"${OUTPUT_FRAMEWORK_BUNDLE_DIR}.dSYM/Contents/Resources/DWARF/${BIN_NAME}\""
+  eval "${LIPO_CMD}"
+  
+  # Swift modules:
+  for SWIFT_MODULE in "${OUTPUT_SWIFT_MODULES[@]}"
+    do
+      cp -a "${SWIFT_MODULE}/" "${OUTPUT_FRAMEWORK_BUNDLE_DIR}/Modules/${BIN_NAME}.swiftmodule/"
+  done
 fi
 
 # Cleanup:
