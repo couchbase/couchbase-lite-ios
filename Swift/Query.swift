36df2fb3c3863cb85c5bd2003a26f9cba083b216
Pasin Suriyentrakorn <pasin@couchbase.com>
2017-05-15 14:24:19 -0700

    Update Swift Query API comment
    Added documentation for explain().

--- Swift/Query.swift
+++ Swift/Query.swift
@@ -14,39 +14,50 @@ import Foundation
 public class Query {
     
     /** Create a SELECT ALL (*) statement instance. You can then call the Select instance's
-     methods such as from() method to construct the complete Query instance. */
+        methods such as from() method to construct the complete Query instance. */
     public static func select() -> Select {
         return Select(impl: CBLQuerySelect.all(), distict: false)
     }
     
     /** Create a SELECT DISTINCT ALL (*) statement instance. You can then call the Select instance's
-     methods such as from() method to construct the complete Query instance. */
+        methods such as from() method to construct the complete Query instance. */
     public static func selectDistinct() -> Select {
         return Select(impl: CBLQuerySelect.all(), distict: true)
     }
     
     /** Runs the query. The returning an enumerator that returns result rows one at a time.
-     You can run the query any number of times, and you can even have multiple enumerators active at
-     once.
-     The results come from a snapshot of the database taken at the moment -run: is called, so they
-     will not reflect any changes made to the database afterwards. */
+        You can run the query any number of times, and you can even have multiple enumerators active at
+        once.
+        The results come from a snapshot of the database taken at the moment -run: is called, so they
+        will not reflect any changes made to the database afterwards. */
     public func run() throws -> QueryIterator {
         guard let database = database else {
             throw CouchbaseLiteError.invalidQuery
         }
         
         if queryImpl == nil {
-            try query()
+            try prepareQuery()
         }
         
         return try QueryIterator(database: database, enumerator: queryImpl!.run())
     }
     
+    /** Returns a string describing the implementation of the compiled query.
+        This is intended to be read by a developer for purposes of optimizing the query, especially
+        to add database indexes. It's not machine-readable and its format may change.
+     
+        As currently implemented, the result is two or more lines separated by newline characters:
+        * The first line is the SQLite SELECT statement.
+        * The subsequent lines are the output of SQLite's "EXPLAIN QUERY PLAN" command applied to that
+          statement; for help interpreting this, see https://www.sqlite.org/eqp.html . The most
+          important thing to know is that if you see "SCAN TABLE", it means that SQLite is doing a
+          slow linear scan of the documents instead of using an index.
+        @param outError If an error occurs, it will be stored here if this parameter is non-NULL.
+        @return a string describing the implementation of the compiled query. */
     public func explain() throws -> String {
         if queryImpl == nil {
-            try query()
+            try prepareQuery()
         }
-
         return try queryImpl!.explain()
     }
 
@@ -68,7 +79,7 @@ public class Query {
     
     init() { }
     
-    func query() throws {
+    func prepareQuery() throws {
         guard let selectImpl = selectImpl, let fromImpl = fromImpl else {
             throw CouchbaseLiteError.invalidQuery
         }
