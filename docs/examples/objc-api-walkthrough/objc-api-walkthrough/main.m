56a4c4693148da5605985a193d2a49df0e1a3600
James Nocentini <james.nocentini@gmail.com>
2017-07-11 22:52:21 +0100

    objc-api-walkthrough project (#1814)
    * add initial project.
    
    * update per review and run against examples DB12.

---
+++ docs/examples/objc-api-walkthrough/objc-api-walkthrough/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc-api-walkthrough
+//
+//  Created by James Nocentini on 27/06/2017.
+//  Copyright © 2017 couchbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
