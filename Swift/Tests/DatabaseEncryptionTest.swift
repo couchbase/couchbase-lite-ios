0bd48861df0d98e45ea151efe8ef1796db38c3d9
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-01-30 01:36:11 -0800

    Fixed DatabaseEncryptionTest
    Enabled / Disabled the test based on COUCHBASE_ENTERPRISE preprocessor as appropriate.

--- Swift/Tests/DatabaseEncryptionTest.swift
+++ Swift/Tests/DatabaseEncryptionTest.swift
@@ -38,15 +38,21 @@ class DatabaseEncryptionTest: CBLTestCase {
         seekrit = nil
         
         // Try to reopen with password (fails):
+    #if COUCHBASE_ENTERPRISE
         expectError(domain: CBLErrorDomain, code: CBLErrorUnreadableDatabase) {
             try _ = self.openSeekrit(password: "wrong")
         }
-        
+    #else
+        expectError(domain: CBLErrorDomain, code: CBLErrorUnsupported) {
+            try _ = self.openSeekrit(password: "wrong")
+        }
+    #endif
         // Reopen with no password:
         seekrit = try openSeekrit(password: nil)
         XCTAssertEqual(seekrit!.count, 1)
     }
     
+#if COUCHBASE_ENTERPRISE
     func testEncryptedDatabase() throws {
         // Create encrypted database:
         seekrit = try openSeekrit(password: "letmein")
@@ -220,4 +226,5 @@ class DatabaseEncryptionTest: CBLTestCase {
             i = i + 1
         }
     }
+#endif
 }
