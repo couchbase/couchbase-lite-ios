8e336c06b1bde85b2e741439e9cb0784354e3f00
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-02-09 15:21:38 -0800

    Removed all 1.x files for merging feature/2.0 into

--- Source/API/CBLRegisterJSViewCompiler.h
+++
@@ -1,9 +0,0 @@
-//
-//  CBLRegisterJSViewCompiler.h
-//  CouchbaseLite
-//
-//  Created by Chris Anderson on 7/10/13.
-//  Copyright (c) 2013 Couchbase, Inc. All rights reserved.
-//
-
-void CBLRegisterJSViewCompiler(void);
