f7bf1ae72520305966e9da06443307379c6d6fcc
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-03-16 15:11:40 -0700

    Map network domain errors to CBL errors
    * Instead of mapping network domain errors to CFNetwork errors, mapping them to CBL domain errors with the base error of 5000.
    * Not mapping POSIX network error-> keep them in POSIX domains.
    
    #2098

--- Objective-C/CBLErrors.h
+++ Objective-C/CBLErrors.h
@@ -57,26 +57,40 @@ NS_ERROR_ENUM(CBLErrorDomain) {
     CBLErrorCantUpgradeDatabase,    // Database can't be upgraded (might be unsupported dev version)
     // Note: These are equivalent to the C4Error codes declared in LiteCore's c4Base.h
 
-    CBLErrorHTTPBase                = 10000,    // ---- HTTP status codes start here
-    CBLErrorHTTPAuthRequired        = 10401,    // Missing or incorrect user authentication
-    CBLErrorHTTPForbidden           = 10403,    // User doesn't have permission to access resource
-    CBLErrorHTTPNotFound            = 10404,    // Resource not found
-    CBLErrorHTTPConflict            = 10409,    // Update conflict
-    CBLErrorHTTPProxyAuthRequired   = 10407,    // HTTP proxy requires authentication
-    CBLErrorHTTPEntityTooLarge      = 10413,    // Data is too large to upload
-    CBLErrorHTTPImATeapot           = 10418,    // HTCPCP/1.0 error (RFC 2324)
-    CBLErrorHTTPInternalServerError = 10500,    // Something's wrong with the server
-    CBLErrorHTTPNotImplemented      = 10501,    // Unimplemented server functionality
-    CBLErrorHTTPServiceUnavailable  = 10503,    // Service is down temporarily(?)
+    CBLErrorNetworkBase               = 5000,     // ---- Network error codes start here
+    CBLErrorDNSFailure                = 5001,     // DNS lookup failed
+    CBLErrorUnknownHost               = 5002,     // DNS server doesn't know the hostname
+    CBLErrorTimeout                   = 5003,     // Socket timeout during an operation
+    CBLErrorInvalidURL                = 5004,     // The provided url is not valid
+    CBLErrorTooManyRedirects          = 5005,     // Too many HTTP redirects for the HTTP client to handle
+    CBLErrorTLSHandshakeFailed        = 5006,     // Failure during TLS handshake process
+    CBLErrorTLSCertExpired            = 5007,     // The provided TLS certificate has expired
+    CBLErrorTLSCertUntrusted          = 5008,     // Cert isn't trusted for other reason
+    CBLErrorTLSClientCertRequired     = 5009,     // A required client certificate was not provided
+    CBLErrorTLSClientCertRejected     = 5010,     // Client certificate was rejected by the server
+    CBLErrorTLSCertUnknownRoot        = 5011,     // Self-signed cert, or unknown anchor cert
+    CBLErrorInvalidRedirect           = 5012,     // Attempted redirect to invalid replication endpoint by server
+    
+    CBLErrorHTTPBase                  = 10000,    // ---- HTTP status codes start here
+    CBLErrorHTTPAuthRequired          = 10401,    // Missing or incorrect user authentication
+    CBLErrorHTTPForbidden             = 10403,    // User doesn't have permission to access resource
+    CBLErrorHTTPNotFound              = 10404,    // Resource not found
+    CBLErrorHTTPConflict              = 10409,    // Update conflict
+    CBLErrorHTTPProxyAuthRequired     = 10407,    // HTTP proxy requires authentication
+    CBLErrorHTTPEntityTooLarge        = 10413,    // Data is too large to upload
+    CBLErrorHTTPImATeapot             = 10418,    // HTCPCP/1.0 error (RFC 2324)
+    CBLErrorHTTPInternalServerError   = 10500,    // Something's wrong with the server
+    CBLErrorHTTPNotImplemented        = 10501,    // Unimplemented server functionality
+    CBLErrorHTTPServiceUnavailable    = 10503,    // Service is down temporarily(?)
 
-    CBLErrorWebSocketBase             = 11000,  // ---- WebSocket status codes start here
-    CBLErrorWebSocketGoingAway        = 11001,  // Peer has to close, e.g. because host app is quitting
-    CBLErrorWebSocketProtocolError    = 11002,  // Protocol violation: invalid framing data
-    CBLErrorWebSocketDataError        = 11003,  // Message payload cannot be handled
-    CBLErrorWebSocketAbnormalClose    = 11006,  // TCP socket closed unexpectedly
-    CBLErrorWebSocketBadMessageFormat = 11007,  // Unparseable WebSocket message
-    CBLErrorWebSocketPolicyError      = 11008,  // Message violated unspecified policy
-    CBLErrorWebSocketMessageTooBig    = 11009,  // Message is too large for peer to handle
-    CBLErrorWebSocketMissingExtension = 11010,  // Peer doesn't provide a necessary extension
-    CBLErrorWebSocketCantFulfill      = 11011,  // Can't fulfill request due to "unexpected condition"
+    CBLErrorWebSocketBase             = 11000,    // ---- WebSocket status codes start here
+    CBLErrorWebSocketGoingAway        = 11001,    // Peer has to close, e.g. because host app is quitting
+    CBLErrorWebSocketProtocolError    = 11002,    // Protocol violation: invalid framing data
+    CBLErrorWebSocketDataError        = 11003,    // Message payload cannot be handled
+    CBLErrorWebSocketAbnormalClose    = 11006,    // TCP socket closed unexpectedly
+    CBLErrorWebSocketBadMessageFormat = 11007,    // Unparseable WebSocket message
+    CBLErrorWebSocketPolicyError      = 11008,    // Message violated unspecified policy
+    CBLErrorWebSocketMessageTooBig    = 11009,    // Message is too large for peer to handle
+    CBLErrorWebSocketMissingExtension = 11010,    // Peer doesn't provide a necessary extension
+    CBLErrorWebSocketCantFulfill      = 11011,    // Can't fulfill request due to "unexpected condition"
 };
