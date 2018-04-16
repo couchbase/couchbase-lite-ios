6711fcc5fde3a64cfa3a75b09c94ab56156d85f0
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-04-16 15:48:50 -0700

    Re-fix #2140 that the completionHanlder when reading data from stream was called in loop
    Detect if the data has zero length and atEOF is true, then close the socket with kWebSocketCloseProtocolError error.

--- Objective-C/Internal/Replicator/CBLWebSocket.mm
+++ Objective-C/Internal/Replicator/CBLWebSocket.mm
@@ -238,7 +238,12 @@ static void doCompletedReceive(C4Socket* s, size_t byteCount) {
         if ([self checkError: error])
             return;
         
-        if (!CFHTTPMessageAppendBytes(httpResponse, (const UInt8*)data.bytes, data.length)) {
+        // https://github.com/couchbase/couchbase-lite-ios/issues/2140
+        // In some condition that remote host cannot be reached (TIC 1:57), the
+        // completionHandler could be called with 0 bytes data in indefinite loop;
+        // explicitly check the condition and close the socket with error.
+        if ((data.length == 0 && atEOF) ||
+            !CFHTTPMessageAppendBytes(httpResponse, (const UInt8*)data.bytes, data.length)) {
             // Error reading response!
             [self didCloseWithCode: kWebSocketCloseProtocolError
                             reason: @"Unparseable HTTP response"];
