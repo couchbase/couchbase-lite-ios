ababc38d8a476085726bd6980099a37f62da557b
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-06-18 16:58:44 -0700

    Enable build tool generation in generate_release_zip.sh
    This was fixed by https://github.com/couchbase/couchbase-lite-core/commit/516b07ef43313df4c583fd2dbfacc74d53d52b5e.
    
    Reference: https://github.com/couchbase/couchbase-lite-core/issues/518

--- Scripts/generate_release_zip.sh
+++ Scripts/generate_release_zip.sh
@@ -119,7 +119,7 @@ echo "Build Development Tools ..."
 TOOLS_DIR="$BUILD_DIR/Tools"
 mkdir "$TOOLS_DIR"
 cp vendor/couchbase-lite-core/tools/README.md "$TOOLS_DIR"
-#vendor/couchbase-lite-core/Xcode/build_tool.sh -t 'cblite' -o "$TOOLS_DIR" -v "$VERSION"
+vendor/couchbase-lite-core/Xcode/build_tool.sh -t 'cblite' -o "$TOOLS_DIR" -v "$VERSION"
 
 # Objective-C
 echo "Make Objective-C framework zip file ..."
