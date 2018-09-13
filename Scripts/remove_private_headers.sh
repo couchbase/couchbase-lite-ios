a745ba2711bdc774cfa1fe6afd61f39f9b4e4507
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-09-13 16:03:54 -0700

    Fix remove_private_headers.sh to work with XCode10
    With XCode10, we can modify the output MODULE_MAP_FILE directly.

--- Scripts/remove_private_headers.sh
+++ Scripts/remove_private_headers.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-MODULE_MAP_FILE=${TARGET_TEMP_DIR}/module.modulemap
 FRAMEWORK_DIR=${TARGET_BUILD_DIR}/${PRODUCT_NAME}${WRAPPER_SUFFIX}
+MODULE_MAP_FILE=${FRAMEWORK_DIR}/Modules/module.modulemap
 
 # Remove private headers from module.modulemap file:
 perl -i -0pe "s/module Private {[\s\S]*?}/module Private { }/" "${MODULE_MAP_FILE}"
