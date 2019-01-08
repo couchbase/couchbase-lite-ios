18b18e857ff65ffdcf369117cbc71574f3b87583
jayahariv <jayahariv88@gmail.com>
2019-01-08 14:15:28 -0800

    test: check default log format
    * check default log format in ObjC

--- Objective-C/Tests/LogTest.m
+++ Objective-C/Tests/LogTest.m
@@ -75,17 +75,23 @@
 }
 
 
-- (NSArray<NSURL*>*) logsInDirectory: (nullable NSString*)directory {
-    if (directory == nil) {
-        directory = CBLDatabase.log.file.directory;
-    }
+- (NSArray<NSURL*>*) getLogsInDirectory: (nullable NSString*)directory
+                             properties: (nullable NSArray<NSURLResourceKey>*)keys
+                           onlyInfoLogs: (BOOL)onlyInfo {
+    directory = directory ? directory : CBLDatabase.log.file.directory;
+    NSURL* path = [NSURL URLWithString: directory];
+    AssertNotNil(path);
+    
     NSError* error;
-    NSDirectoryEnumerationOptions options = NSDirectoryEnumerationSkipsSubdirectoryDescendants | NSDirectoryEnumerationSkipsHiddenFiles;
-    NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtURL: [NSURL URLWithString: directory]
-                                                   includingPropertiesForKeys: @[]
-                                                                      options: options
+    NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtURL: path
+                                                   includingPropertiesForKeys: keys ? keys : @[]
+                                                                      options: 0
                                                                         error: &error];
-    NSPredicate* predicate = [NSPredicate predicateWithFormat: @"pathExtension == 'cbllog'"];
+    NSString* format = @"pathExtension == 'cbllog'";
+    if (onlyInfo) {
+        format = [NSString stringWithFormat: @"%@ && lastPathComponent BEGINSWITH 'cbl_info_'", format];
+    }
+    NSPredicate* predicate = [NSPredicate predicateWithFormat: format];
     return [files filteredArrayUsingPredicate: predicate];
 }
 
@@ -153,10 +159,43 @@
 - (void) testDefaultLocation {
     CBLLogInfo(Database, @"TEST INFO");
     
-    NSArray* files = [self logsInDirectory: nil];
-    
+    NSArray* files = [self getLogsInDirectory: nil properties: nil onlyInfoLogs: NO];
     Assert(files.count >= 5, "because there should be at least 5 log entries in the folder");
 }
+
+
+- (void) testDefaultLogFormat {
+    CBLDatabase.log.file.usePlainText = NO;
+    CBLLogInfo(Database, @"TEST INFO");
+    
+    NSArray* files = [self getLogsInDirectory: nil
+                                   properties: @[NSFileModificationDate]
+                                 onlyInfoLogs: YES];
+    NSArray* sorted = [files sortedArrayUsingComparator:^NSComparisonResult(NSURL* url1, NSURL* url2) {
+        NSError* err;
+        NSDate *date1 = nil;
+        [url1 getResourceValue: &date1
+                        forKey: NSURLContentModificationDateKey
+                         error: &err];
+        
+        NSDate* date2 = nil;
+        [url2 getResourceValue: &date2
+                        forKey: NSURLContentModificationDateKey
+                         error: &err];
+        return [date1 compare: date2];
+    }];
+    
+    NSURL* last = [sorted lastObject];
+    AssertNotNil(last);
+    
+    NSError* error;
+    NSFileHandle* sourceFileHandle = [NSFileHandle fileHandleForReadingFromURL: last error: &error];
+    NSData* begainData = [sourceFileHandle readDataOfLength: 4];
+    Byte *bytes = (Byte *)[begainData bytes];
+    Assert(bytes[0] == 0xcf && bytes[1] == 0xb2 && bytes[2] == 0xab && bytes[3] == 0x1b,
+           @"because the log should be in binary format");
+}
+
 
 @end
 
