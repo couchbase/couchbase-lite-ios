312e0a621fc3e4f02f0f299fa920eebeb169dfd8
Pasin Suriyentrakorn <pasin@couchbase.com>
2019-01-14 19:51:43 -0800

    Implement a new file logger API
    - Implemented a new file logger API a refactor log file related settings into a single configuration file, LogFileConfiguration.
    
    - Updated unit tests.

--- Swift/FileLogger.swift
+++ Swift/FileLogger.swift
@@ -19,48 +19,28 @@
 
 import Foundation
 
-/// File logger used for writing log messages to files. The binary file format will
-/// be used by default. To change the file format to plain text, set the usePlainText
-/// property to true.
+/// File logger used for writing log messages to files. To enable the file logger,
+/// setup the log file configuration and specify the log level as desired.
 public class FileLogger {
     
-    private static let defaultMaxSize = UInt64(500 * 1024)
+    /// The log file configuration for configuring the log directory, file format, and rotation
+    /// policy. The config property is nil by default. Setting the config property to nil will
+    /// disable the file logging.
+    public var config: LogFileConfiguration? {
+        didSet {
+            CBLDatabase.log().file.config = config?.toImpl()
+        }
+    }
     
     /// The minimum log level of the log messages to be logged. The default log level for
-    /// file logger is warning.
-    public var level: LogLevel = .info {
+    /// file logger is none which means no logging.
+    public var level: LogLevel = .none {
         didSet {
             CBLDatabase.log().file.level = CBLLogLevel(rawValue: UInt(level.rawValue))!
         }
     }
     
-    /// The directory to store the log files.
-    public var directory: String = CBLDatabase.log().file.directory {
-        didSet {
-            CBLDatabase.log().file.directory = directory
-        }
-    }
-    
-    /// To use plain text file format instead of the default binary format.
-    public var usePlainText: Bool = false {
-        didSet {
-            CBLDatabase.log().file.usePlainText = usePlainText
-        }
-    }
-    
-    /// The maximum size of a log file before being rotation. The default is 1024 bytes.
-    public var maxSize: UInt64 = defaultMaxSize {
-        didSet {
-            CBLDatabase.log().file.maxSize = maxSize
-        }
-    }
-    
-    /// The maximum number of rotated log files to keep. The default is 1 which means no rotation.
-    public var maxRotateCount: Int = 1 {
-        didSet(value) {
-            CBLDatabase.log().file.maxRotateCount = value
-        }
-    }
+    // MARK: Internal
     
     init() { }
 }
