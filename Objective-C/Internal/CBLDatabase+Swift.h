a475c24c99ddb4c8c845342c5070d6ac90de2f74
Pasin Suriyentrakorn <pasin@couchbase.com>
2019-01-23 21:57:49 -0800

    Add warning when file logging is not configured
    * Added warning when file logging is not configured. The warning will display only once when a database instance is created. The warning message will not show more than once even there are multiple databases created.
    
    * I have tested it manually for both Swift and Objective-C. I was not able to write a unit test as we always have a default testing database opened in the base test class’s setup.
    
    #2344

---
+++ Objective-C/Internal/CBLDatabase+Swift.h
@@ -0,0 +1,15 @@
+//
+//  CBLDatabase+Swift.h
+//  CouchbaseLite
+//
+//  Created by Pasin Suriyentrakorn on 1/23/19.
+//  Copyright © 2019 Couchbase. All rights reserved.
+//
+
+#import "CBLDatabase.h"
+
+@interface CBLDatabase ()
+
++ (void) checkFileLogging: (BOOL)swift;
+
+@end
