cb7615eb9c2e37f1d25306b91b567b9d5e2824c4
jayahariv <jayahariv88@gmail.com>
2019-04-03 17:35:22 -0700

    unit: check parse date
    * check parse date with a known date string and double return value.

--- Objective-C/Tests/MiscTest.m
+++ Objective-C/Tests/MiscTest.m
@@ -19,6 +19,7 @@
 
 #import "CBLTestCase.h"
 #import "CBLJSON.h"
+#import "CBLParseDate.h"
 
 @interface MiscTest : CBLTestCase
 
@@ -37,5 +38,13 @@
     AssertEqualObjects(dateStr2, dateStr1);
 }
 
+#pragma mark - Parse Date
+
+- (void) testCBLParseISO8601Date {
+    AssertEqual(CBLParseISO8601Date("1970-01-01T00:00:01.000Z"), 1.0);
+    AssertEqual(CBLParseISO8601Date("1970-01-01T00:00:00Z"), 0.0);
+    AssertEqual(CBLParseISO8601Date("2018-10-23T11:33:01-0700"), 1540319581);
+    AssertEqual(CBLParseISO8601Date("2020-02-29T23:59:59.000000+0000"), 1583020799);
+}
 
 @end
