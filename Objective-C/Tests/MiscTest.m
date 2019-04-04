d9ff405623243cbb3d7114731646c3af7905d701
jayahariv <jayahariv88@gmail.com>
2019-04-03 18:02:18 -0700

    ref: remove repeated test

--- Objective-C/Tests/MiscTest.m
+++ Objective-C/Tests/MiscTest.m
@@ -42,7 +42,6 @@
 
 - (void) testCBLParseISO8601Date {
     AssertEqual(CBLParseISO8601Date("1970-01-01T00:00:01.000Z"), 1.0);
-    AssertEqual(CBLParseISO8601Date("1970-01-01T00:00:00Z"), 0.0);
     AssertEqual(CBLParseISO8601Date("2018-10-23T11:33:01-0700"), 1540319581);
     AssertEqual(CBLParseISO8601Date("2020-02-29T23:59:59.000000+0000"), 1583020799);
 }
