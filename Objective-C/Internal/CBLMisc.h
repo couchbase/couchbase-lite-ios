87684eb6ce247335f5ca9360a0592ef1b6f7e4c4
jayahariv <jayahariv88@gmail.com>
2019-04-04 17:05:10 -0700

    * remove unused methods

--- Objective-C/Internal/CBLMisc.h
+++ Objective-C/Internal/CBLMisc.h
@@ -27,14 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** Generates UUID. */
 NSString* __nullable CBLCreateUUID( void );
-
-/** Writes a hex dump of the bytes to the output string.
- Returns a pointer to the end of the string (where it writes a null.) */
-char* CBLAppendHex( char *dst, const void* bytes, size_t length);
-
-/** Generates a hex dump of a sequence of bytes.
- The result is lowercase. This is important for CouchDB compatibility. */
-NSString* CBLHexFromBytes( const void* bytes, size_t length);
     
 /** Returns YES if this error appears to be due to a creating a file/dir that already exists. */
 BOOL CBLIsFileExistsError( NSError* error );
