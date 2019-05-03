9119d8eedeb3ed21c885483bbab0f24d33b6fd02
Jayahari Vavachan <jayahariv88@gmail.com>
2019-05-03 15:28:49 -0700

    review comments
    * internal interface without name for consistency

--- Objective-C/Internal/Replicator/CBLConflict+Internal.h
+++ Objective-C/Internal/Replicator/CBLConflict+Internal.h
@@ -21,7 +21,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CBLConflict (Internal)
+@interface CBLConflict ()
 
 - (instancetype) initWithLocalDocument: (nullable CBLDocument*)localDoc
                         remoteDocument: (nullable CBLDocument*)remoteDoc;
