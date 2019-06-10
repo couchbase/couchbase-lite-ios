ad2c1cb0c6b3c3bf826b6b4502df2b6dfd95fbc6
jayahariv <jayahariv88@gmail.com>
2019-06-10 11:08:22 -0700

    fix: replicator queue add
    * changed the replicator to use an independent serial queue instead of the database queue.
    * added test to verify the database operations are not blocked due to this serial queue.
    * reverted the replicator to use separate queue

--- Objective-C/Internal/CBLReplicator+Internal.h
+++ Objective-C/Internal/CBLReplicator+Internal.h
@@ -55,7 +55,6 @@ typedef NS_ENUM(uint32_t, CBLCustomWebSocketCloseCode) {
 @property (atomic) BOOL suspended;
 @property (nonatomic) MYBackgroundMonitor* bgMonitor;
 @property (readonly, atomic) dispatch_queue_t dispatchQueue;
-@property (readonly, atomic) dispatch_queue_t conflictResolverQueue;
 
 @end
 
