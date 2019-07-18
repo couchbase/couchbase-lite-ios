7d237f440d579c40e4d6071da3de5d879fdd3258
jayahariv <jayahariv88@gmail.com>
2019-07-18 13:29:25 -0700

    * add iphone simulator archive * correct it

--- Scripts/build_xcframework.sh
+++ Scripts/build_xcframework.sh
@@ -4,7 +4,7 @@ set -e
 
 function usage
 {
-  echo "Usage: ${0} -s <Scheme: \"CBL ObjC\" or \"CBL Swift\"> -o <Output Directory> [-v <Version (<Version Number>[-<Build Number>])>] [--quiet]"
+  echo "Usage: ${0} -s <Scheme: \"CBL ObjC\" or \"CBL Swift\"> [-c <Configuration Name, default is 'Release'>] -o <Output Directory> [-v <Version (<Version Number>[-<Build Number>])>] [--quiet]"
 }
 
 while [[ $# -gt 0 ]]
@@ -13,6 +13,10 @@ do
   case $key in
     -s)
     SCHEME=${2}
+    shift
+    ;;
+    -c)
+    CONFIGURATION=${2}
     shift
     ;;
     -o)
@@ -40,7 +44,11 @@ then
   exit 4
 fi
 
-CONFIGURATION="Release"
+if [ -z "$CONFIGURATION" ]
+then
+  CONFIGURATION="Release"
+fi
+
 echo "Scheme: ${SCHEME}"
 echo "Configuration : ${CONFIGURATION}"
 echo "Output Directory: ${OUTPUT_DIR}"
@@ -77,23 +85,26 @@ then
 fi
 
 # archive
-DESTINATIONS=("iOS" "macOS")
-INDEX=1
+DESTINATIONS=("iOS Simulator" "iOS" "macOS")
 for DESTINATION in "${DESTINATIONS[@]}"
 do
   echo "Exporting ${DESTINATION}!!"
-  ARCHIVE_PATH=${OUTPUT_DIR}/${DESTINATION}/${BIN_NAME}.xcarchive
-  echo $ARCHIVE_PATH
+  FOLDER_NAME=$(echo ${DESTINATION} | sed 's/ /_/g')
+echo ${FOLDER_NAME}
+  ARCHIVE_PATH=${OUTPUT_DIR}/${FOLDER_NAME}/${BIN_NAME}.xcarchive
+echo ${ARCHIVE_PATH}
   xcodebuild archive -scheme "${SCHEME}" -configuration "${CONFIGURATION}" -destination "generic/platform=${DESTINATION}" ${BUILD_VERSION} ${BUILD_NUMBER}   -archivePath ${ARCHIVE_PATH} "ONLY_ACTIVE_ARCH=NO" "BITCODE_GENERATION_MODE=bitcode" "CODE_SIGNING_REQUIRED=NO" "CODE_SIGN_IDENTITY=" "clean"  ${QUIET} "SKIP_INSTALL=NO"
   echo "Exported ${DESTINATION}!!"
-  ((INDEX+=1))
 done
 
 FRAMEWORK_LOCATION=${BIN_NAME}.xcarchive/Products/Library/Frameworks/${BIN_NAME}.framework
 
 # create xcframework
-xcodebuild -create-xcframework -output ${OUTPUT_DIR}/${BIN_NAME}.xcframework -framework ${OUTPUT_DIR}/iOS/${FRAMEWORK_LOCATION} -framework ${OUTPUT_DIR}/macOS/${FRAMEWORK_LOCATION}
+xcodebuild -create-xcframework -output ${OUTPUT_DIR}/${BIN_NAME}.xcframework -framework ${OUTPUT_DIR}/iOS/${FRAMEWORK_LOCATION} -framework ${OUTPUT_DIR}/macOS/${FRAMEWORK_LOCATION} -framework ${OUTPUT_DIR}/iOS_Simulator/${FRAMEWORK_LOCATION}
 
 # remove all related files
-rm -rf ${OUTPUT_DIR}/iOS
-rm -rf ${OUTPUT_DIR}/macOS
+for DESTINATION in "${DESTINATIONS[@]}"
+do
+  FOLDER_NAME=$(echo ${DESTINATION} | sed 's/ /_/g')
+  rm -rf ${OUTPUT_DIR}/${FOLDER_NAME}
+done
