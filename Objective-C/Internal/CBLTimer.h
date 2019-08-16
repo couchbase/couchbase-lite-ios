b5b6bdf0715cbffacbd12e87ea9d9e29f77917b0
jayahariv <jayahariv88@gmail.com>
2019-08-16 15:41:32 -0700

    feat: add dispatch source set timer
    * create a wrapper for dispatch source set timer, for all cbl timer tasks.
    * for a test trial, check by replacing the document expiry code block.

---
+++ Objective-C/Internal/CBLTimer.h
@@ -0,0 +1,33 @@
+//
+//  CBLTimer.h
+//  CouchbaseLite
+//
+//  Copyright (c) 2019 Couchbase, Inc All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CBLTimer : NSObject
+
++ (dispatch_source_t) scheduleIn: (nullable dispatch_queue_t)queue
+                           after: (double)seconds
+                           block: (dispatch_block_t)block;
+
+@end
+
+NS_ASSUME_NONNULL_END
