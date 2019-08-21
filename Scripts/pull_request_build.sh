9604756282dfb75c60169bcb3a59ed9f2a3c4516
Kim Trang <kim.trang@couchbase.com>
2019-08-20 17:56:30 -0700

    Add pull_request_build.sh

---
+++ Scripts/pull_request_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+cd couchbase-lite-ios
+SCHEMES=("CBL ObjC" "CBL Swift")
+
+for SCHEME in "${SCHEMES[@]}"
+
+do
+  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone XS"
+
+done
