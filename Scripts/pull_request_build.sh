8d2682117e53ec300525116e15c49666096262d5
Jayahari Vavachan <jayahariv88@gmail.com>
2019-10-08 17:07:57 -0700

    feat: include enterprise tests

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -xe
 
-cd couchbase-lite-ios
-SCHEMES=("CBL ObjC" "CBL Swift")
-
-for SCHEME in "${SCHEMES[@]}"
-
+cd couchbase-lite-ios-ee/couchbase-lite-ios
+EDITIONS=("EE" "CE")
+for EDITION in "${EDITIONS[@]}"
 do
-  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11"
-
+  if [ ${EDITION} = "EE" ]
+  then
+    EE="-EE"
+  else 
+    EE=""
+  fi
+  
+  SCHEMES=("CBL${EE} ObjC" "CBL${EE} Swift")
+  for SCHEME in "${SCHEMES[@]}"
+  do
+    xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11"
+  done
 done
