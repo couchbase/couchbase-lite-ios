23623d2d85e12a86a74ced2c9215badafb12ac51
Jayahari Vavachan <jayahariv88@gmail.com>
2019-10-27 19:04:20 -0700

    simply
    more readable

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash -xe
 
 cd couchbase-lite-ios
-EDITIONS=("EE" "CE")
-for EDITION in "${EDITIONS[@]}"
+
+SCHEMES=("CBL-EE ObjC" "CBL-EE Swift" "CBL ObjC" "CBL Swift")
+for SCHEME in "${SCHEMES[@]}"
 do
-  if [ ${EDITION} = "EE" ]
-  then
-    EE="-EE"
-  else 
-    EE=""
-  fi
-  
-  SCHEMES=("CBL${EE} ObjC" "CBL${EE} Swift")
-  for SCHEME in "${SCHEMES[@]}"
-  do
-    xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11"
-  done
+  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11"
 done
