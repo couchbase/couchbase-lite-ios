bb04de72847415936116257ab8d2a711b906ac29
Jayahari Vavachan <jayahariv88@gmail.com>
2019-10-31 11:03:44 -0700

    CBL-449: IsDocumentPending ObjC (#2565)
    * implement the isDocumentPending method

--- Objective-C/CBLReplicator.h
+++ Objective-C/CBLReplicator.h
@@ -153,6 +153,20 @@ typedef struct {
  */
 - (void) removeChangeListenerWithToken: (id<CBLListenerToken>)token;
 
+
+
+
+/**
+ Checks if the document with the given ID has revisions pending push.  This API is a snapshot and results may
+ change between the time the call was made and the time the call returns.
+
+ @param documentID The ID of the document to check
+ @param error error On return, the error if any.
+ @return true if the document has one or more revisions pending, false otherwise
+ 
+ */
+- (BOOL) isDocumentPending: (NSString*)documentID error: (NSError**)error;
+
 @end
 
 
