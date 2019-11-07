d801381521cd812014deafe10eb065303d1b703d
jayahariv <jayahariv88@gmail.com>
2019-11-07 10:04:57 -0800

    * remove the condition check from replicator busy state, since it cannot be guaranteed

--- Objective-C/Tests/ReplicatorTest+PendingDocIds.m
+++ Objective-C/Tests/ReplicatorTest+PendingDocIds.m
@@ -66,14 +66,9 @@
 
     __block id<CBLListenerToken> token;
     __block CBLReplicator* replicator;
-    __block BOOL finishReplicating = NO;
     __weak typeof(self) wSelf = self;
     [self run: config reset: NO errorCode: 0 errorDomain: nil onReplicatorReady: ^(CBLReplicator* r) {
         replicator = r;
-        
-        [replicator addDocumentReplicationListener:^(CBLDocumentReplication * docRepl) {
-            finishReplicating = YES;
-        }];
         
         token = [replicator addChangeListener: ^(CBLReplicatorChange* change) {
             __strong id strongSelf = wSelf;
@@ -87,9 +82,6 @@
             
             if (change.status.activity == kCBLReplicatorConnecting) {
                 XCTAssert(docIds.count == 5u);
-            } else if (change.status.activity == kCBLReplicatorBusy) {
-                if (!finishReplicating)
-                    XCTAssert(docIds.count != 0);
             } else if (change.status.activity == kCBLReplicatorStopped) {
                 XCTAssertEqual(docIds.count, 0);
             }
