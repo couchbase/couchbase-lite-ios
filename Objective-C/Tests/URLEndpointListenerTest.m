0996229750d8247e2451f42a1da932eb7ef5c27e
jayahariv <10448770+jayahariv@users.noreply.github.com>
2020-05-14 13:18:32 -0700

    * include tests for get URLs, but test fails due to wrong scheme in URLs * include tests for getting listenet status, but again fails the active connection count, lite core fails. * added todo comment, and disable test for now

--- Objective-C/Tests/URLEndpointListenerTest.m
+++ Objective-C/Tests/URLEndpointListenerTest.m
@@ -9,8 +9,10 @@
 #import "ReplicatorTest.h"
 #import "CBLURLEndpointListener.h"
 #import "CBLURLEndpointListenerConfiguration.h"
+#import "CollectionUtils.h"
 
 #define kPort 4984
+#define kURL [NSURL URLWithString: $sprintf(@"ws://localhost:%d/otherdb", kPort)]
 
 API_AVAILABLE(macos(10.12), ios(10.0))
 @interface URLEndpointListenerTest : ReplicatorTest
@@ -88,6 +90,62 @@ typedef CBLURLEndpointListener Listener;
     
     // stops
     [list1 stop];
+}
+
+// TODO: https://issues.couchbase.com/browse/CBL-948
+- (void) _testURLs {
+    Config* config = [[Config alloc] initWithDatabase: self.otherDB port: kPort];
+    config.disableTLS = YES;
+    Listener* list = [[Listener alloc] initWithConfig: config];
+    AssertEqual(list.urls.count, 0);
+    
+    // start listener
+    NSError* err = nil;
+    Assert([list startWithError: &err]);
+    AssertNil(err);
+    Assert(list.urls.count != 0);
+    
+    // stops
+    [list stop];
+    AssertEqual(list.urls.count, 0);
+}
+
+// TODO: LiteCore-EE C++ Test "P2P Sync connection count" Failure!
+- (void) _testStatus {
+    CBLDatabase.log.console.level = kCBLLogLevelDebug;
+    Config* config = [[Config alloc] initWithDatabase: self.otherDB port: kPort];
+    config.disableTLS = YES;
+    Listener* list = [[Listener alloc] initWithConfig: config];
+    AssertEqual(list.status.connectionCount, 0);
+    AssertEqual(list.status.activeConnectionCount, 0);
+    
+    // start listener
+    NSError* err = nil;
+    Assert([list startWithError: &err]);
+    AssertNil(err);
+    AssertEqual(list.status.connectionCount, 0);
+    AssertEqual(list.status.activeConnectionCount, 0);
+    
+    [self generateDocumentWithID: @"doc-1"];
+    CBLURLEndpoint* target = [[CBLURLEndpoint alloc] initWithURL: kURL];
+    id rConfig = [self configWithTarget: target type: kCBLReplicatorTypePush continuous: NO];
+    __block Listener* tempListener = list;
+    __block uint64 maxConnectionCount = 0, maxActiveCount = 0;
+    [self run: rConfig reset: NO errorCode: 0 errorDomain: nil onReplicatorReady:^(CBLReplicator * r) {
+        Listener* sListener = tempListener;
+        [r addChangeListener:^(CBLReplicatorChange * change) {
+            maxConnectionCount = MAX(sListener.status.connectionCount, maxConnectionCount);
+            maxActiveCount = MAX(sListener.status.activeConnectionCount, maxActiveCount);
+        }];
+    }];
+    AssertEqual(maxActiveCount, 1);
+    AssertEqual(maxConnectionCount, 1);
+    AssertEqual(self.otherDB.count, 1);
+    
+    // stops
+    [list stop];
+    AssertEqual(list.status.connectionCount, 0);
+    AssertEqual(list.status.activeConnectionCount, 0);
 }
 
 @end
