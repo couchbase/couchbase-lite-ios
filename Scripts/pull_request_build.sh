8d636d484c2f9dde865caebd8e65daadd51de94e
jayahariv <jayahariv88@gmail.com>
2020-06-29 18:02:18 -0700

    fix: scheme name: replace space with underscore

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -2,7 +2,7 @@
 
 cd couchbase-lite-ios
 
-SCHEMES=("CBL-EE ObjC" "CBL-EE Swift" "CBL ObjC" "CBL Swift")
+SCHEMES=("CBL_EE_ObjC" "CBL_EE_Swift" "CBL_ObjC" "CBL_Swift")
 for SCHEME in "${SCHEMES[@]}"
 do
   xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 11"
