c4d5dfb183475bc7c5d7fb6324289d26982bd019
Jayahari Vavachan <jayahariv88@gmail.com>
2020-10-06 19:26:07 -0700

    spm: add package manifest 2.8.0 CE (#2757)
    * add package manifest

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CouchbaseLiteSwift",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_11)
+    ],
+    products: [
+        .library(
+            name: "CouchbaseLiteSwift",
+            targets: ["CouchbaseLiteSwift"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CouchbaseLiteSwift",
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/2.8.0/couchbase-lite-swift_xc_community_2.8.0.zip",
+            checksum: "8acca88ce094967839ed3ae22d9eddc858598853b2a6d9a15d30a915d3c6ddbe"
+        )
+    ]
+)
