7ef36798e45f2c12e747911aa6ca45ec99375c9d
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2021-01-18 20:32:19 -0800

    include catalyst support flag and enable catalyst with xcframework build (#2793)
    * include catalyst support flag and enable catalyst with xcframework build

--- Scripts/build_xcframework.sh
+++ Scripts/build_xcframework.sh
@@ -114,8 +114,7 @@ function xcarchive
   echo "Finished archiving ${DESTINATION}."
 }
 
-# TODO: uncomment when catalyst is ready
-# xcarchive "platform=macOS,arch=x86_64,variant=Mac Catalyst"
+xcarchive "platform=macOS,arch=x86_64,variant=Mac Catalyst"
 xcarchive "generic/platform=iOS Simulator"
 xcarchive "generic/platform=iOS"
 xcarchive "generic/platform=macOS"
