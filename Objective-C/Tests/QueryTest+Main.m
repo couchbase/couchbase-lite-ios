19eb0adef1bbfe11125546622129ad559b11a15c
jayahariv <10448770+jayahariv@users.noreply.github.com>
2021-06-23 23:53:53 -0700

    enable the N1QL with default tableNames

--- Objective-C/Tests/QueryTest+Main.m
+++ Objective-C/Tests/QueryTest+Main.m
@@ -1845,11 +1845,12 @@
     [doc setValue: @"Ice Cream" forKey: @"lastName"];
     [self saveDocument: doc];
     
-// FIXME: once the FROM clause can handle the _ and <dbName>, uncomment this.
-//    [self validateN1QLQuery: $sprintf(@"SELECT firstName, lastName FROM %@", self.db.name)];
-//    [self validateN1QLQuery: @"SELECT firstName, lastName FROM _"]; // should work as expected
-//    [self validateN1QLQuery: @"SELECT firstName, lastName"]; // should throw exception
+    [self validateN1QLQuery: $sprintf(@"SELECT firstName, lastName FROM %@", self.db.name)];
+    [self validateN1QLQuery: @"SELECT firstName, lastName FROM _"];
     [self validateN1QLQuery: @"SELECT firstName, lastName FROM _default"];
+    
+    // FIXME: Should throw error!
+    // [self validateN1QLQuery: @"SELECT firstName, lastName"];
 }
 
 - (void) validateN1QLQuery: (NSString*)queryString {
