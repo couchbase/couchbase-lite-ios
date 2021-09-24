5637b2b46962eb6815dbc89a380e0a0d44a569ad
jayahariv <10448770+jayahariv@users.noreply.github.com>
2021-09-23 19:48:50 -0700

    fix API available for typedef (seems like xcode13 makes it madatory)

--- Objective-C/Tests/URLEndpointListenerTest.m
+++ Objective-C/Tests/URLEndpointListenerTest.m
@@ -58,8 +58,13 @@ NS_ASSUME_NONNULL_END
 
 API_AVAILABLE(macos(10.12), ios(10.0))
 @interface URLEndpointListenerTest : ReplicatorTest
+
+API_AVAILABLE(macos(10.12), ios(10.0))
 typedef CBLURLEndpointListenerConfiguration Config;
+
+API_AVAILABLE(macos(10.12), ios(10.0))
 typedef CBLURLEndpointListener Listener;
+
 @end
 
 @implementation URLEndpointListenerTest {
