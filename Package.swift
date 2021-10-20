f238f8bd2c3ebfe76d791c43f8494fb63faad723
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2021-10-20 06:47:24 +0530

    CBL-2489: Add Swift Package Manifest file for 3.0.0-beta02 CE (#2902)

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.4.2
+import PackageDescription
+
+let package = Package(
+    name: "CouchbaseLiteSwift",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_11)
+    ],
+    products: [
+        .library(
+            name: "CouchbaseLiteSwift",
+            targets: ["CouchbaseLiteSwift"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CouchbaseLiteSwift",
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.0.0-beta02/couchbase-lite-swift_xc_community_3.0.0-beta02.zip",
+            checksum: "ff2ff7ad9180e5916d08e47fa160fa35546c50b12fd6dd834e9e4f48a75a6575"
+        )
+    ]
+)
