184b542358c7d1a8c7b20569a810027fb3031d33
jayahariv <10448770+jayahariv@users.noreply.github.com>
2022-02-17 01:37:40 -0800

    add TLS domain in the mapping section to database domain

--- Objective-C/CBLLog.mm
+++ Objective-C/CBLLog.mm
@@ -82,6 +82,7 @@ static CBLLogDomain toCBLLogDomain(C4LogDomain domain) {
     if (!domainDictionary) {
         domainDictionary = @{ @"DB": @(kCBLLogDomainDatabase),
                               @"SQL": @(kCBLLogDomainDatabase),
+                              @"TLS": @(kCBLLogDomainDatabase), // for both replicator & listener
                               @"Query": @(kCBLLogDomainQuery),
                               @"Sync": @(kCBLLogDomainReplicator),
                               @"SyncBusy": @(kCBLLogDomainReplicator),
