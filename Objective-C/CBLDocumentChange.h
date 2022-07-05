8478c63674ab1d7709e3e0b895cd739f2d431cc4
jayahariv <10448770+jayahariv@users.noreply.github.com>
2022-07-05 02:58:32 -0700

    deprecate the database for DocumentChange

--- Objective-C/CBLDocumentChange.h
+++ Objective-C/CBLDocumentChange.h
@@ -27,7 +27,8 @@
 @property (readonly, nonatomic) CBLCollection* collection;
 
 /** The database. */
-@property (readonly, nonatomic) CBLDatabase* database;
+@property (readonly, nonatomic) CBLDatabase* database
+__deprecated_msg("Use collection instead.");
 
 /** The ID  of the document that changed. */
 @property (readonly, nonatomic) NSString* documentID;
