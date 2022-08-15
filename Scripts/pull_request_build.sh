3d7047a79e7c86a4df52f515cc767008f143e242
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2022-08-16 04:02:54 +0530

    Remove the CE schemes (#2999)
    Remove the CE scheme, since that's already tested via GH Actions

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -2,7 +2,7 @@
 
 cd couchbase-lite-ios
 
-SCHEMES=("CBL_EE_ObjC" "CBL_EE_Swift" "CBL_ObjC" "CBL_Swift")
+SCHEMES=("CBL_EE_ObjC" "CBL_EE_Swift")
 for SCHEME in "${SCHEMES[@]}"
 do
   xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone SE (2nd generation)"
