9e0451c2d847b79b03930544877d69f9111af318
jayahariv <10448770+jayahariv@users.noreply.github.com>
2022-09-15 20:51:32 -0700

    update the simulator to iPhone 8

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -5,5 +5,5 @@ cd couchbase-lite-ios
 SCHEMES=("CBL_EE_ObjC" "CBL_EE_Swift")
 for SCHEME in "${SCHEMES[@]}"
 do
-  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone SE (2nd generation)"
+  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8"
 done
