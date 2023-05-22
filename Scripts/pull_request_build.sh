687f3b4cf5289e50c9ab31097660d07d40371fca
Ming Ho <ming.ho@couchbase.com>
2023-05-22 13:35:08 -0700

    test PR.
    -Ming

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -xe
+set -x
+echo "test PR"
 
-cd couchbase-lite-ios
-
-TEST_SIMULATOR=$(xcrun xctrace list devices 2>&1 | grep -oE 'iPhone.*?[^\(]+' | head -1 | sed 's/Simulator//g' | awk '{$1=$1;print}')
-SCHEMES=("CBL_EE_ObjC" "CBL_EE_Swift")
-
-for SCHEME in "${SCHEMES[@]}"
-do
-  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=${TEST_SIMULATOR}"
-done
