b48ba0e524b884d6455b3f4fab171fdaa3373784
Velicu Vlad <vlad_vel@yahoo.com>
2023-05-26 21:04:13 +0100

    change to weak self and add warning log when coll ref is nil (#3134)

--- Swift/Collection.swift
+++ Swift/Collection.swift
@@ -253,8 +253,12 @@ public final class Collection : CollectionChangeObservable, Indexable, Equatable
     @discardableResult public func addDocumentChangeListener(id: String, queue: DispatchQueue?,
                                    listener: @escaping (DocumentChange) -> Void) -> ListenerToken {
         let token = impl.addDocumentChangeListener(withID: id, queue: queue)
-        { [unowned self] (change) in
-            listener(DocumentChange(database: db,
+        { [weak self] (change) in
+            guard let self = self else {
+                Log.log(domain: .database, level: .warning, message: "Unable to notify changes as the collection object was released")
+                return
+            }
+            listener(DocumentChange(database: self.db,
                                     documentID: change.documentID,
                                     collection: self))
         }
