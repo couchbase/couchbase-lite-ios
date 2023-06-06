6b44fb5f8cd997da529173b910b2d1c6ed28265c
Vlad Velicu <vlad.velicu@couchbase.com>
2023-06-06 18:32:20 +0100

    update script

--- Objective-C/Tests/Support/identity/en_client_cert.sh
+++ Objective-C/Tests/Support/identity/en_client_cert.sh
@@ -1,33 +1,20 @@
 #!/bin/bash -e
-
-echo
-echo "You will be prompted for a password, use your own chosen password and keep entering the same one until told otherwise."
-read -p "Press Enter to continue"
-
-openssl genrsa -aes256 -out test-ca.key 4096
-openssl req -x509 -new -nodes -key test-ca.key -sha256 -days 3650 -out test-ca.crt -subj "/CN=Test CA/C=UK/ST=UK/L=Manchester/O=Couchbase"
-openssl req -new -nodes -out test-node.csr -newkey rsa:4096 -keyout test-node.key -subj "/CN=Test Node/C=UK/ST=UK/L=Manchester/O=Couchbase"
-
+​
+openssl genrsa -out test-ca.key 4096
+openssl req -new -x509 -sha256 -days 3650 -key test-ca.key -out test-ca.pem -subj "/CN=CBL Test CA"
+​
 cat > test-node.v3.ext << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 EOF
-
-openssl x509 -req -in test-node.csr -CA test-ca.crt -CAkey test-ca.key -CAcreateserial -out test-node.crt -days 3650 -sha256 -extfile test-node.v3.ext
-
-rm test-node.v3.ext
-rm test-node.csr
-rm test-ca.srl
-
-echo
-echo "Use the password 123 if you want the exported p12 to work correctly with the tests"
-read -p "Press Enter to continue"
-
-# Use LibreSSL. If using OpenSSL 3.x add -legacy when exporting below
-# https://stackoverflow.com/questions/70431528/mac-verification-failed-during-pkcs12-import-wrong-password-azure-devops
-
-openssl pkcs12 -export -out client.p12 -inkey test-node.key -in test-node.crt
-openssl x509 -in test-ca.crt -out client-ca.der -outform DER
-
-rm -rf test-*
+​
+openssl genrsa -out test-client.key 4096
+openssl req -new -sha256 -key test-client.key -out test-client.pem -subj "/CN=CBL Test"
+openssl x509 -req -sha256 -days 3650 -CA test-ca.pem -CAkey test-ca.key -CAcreateserial -in test-client.pem -out test-client.pem -extfile test-node.v3.ext
+​
+# Add -legacy if you are using openSSL 3.x instead of the default mac LibreSSL 3.x to allow keychain to recongize your password.
+openssl pkcs12 -export -passout pass:123 -out client.p12 -inkey test-client.key -in test-client.pem
+openssl x509 -in test-ca.pem -out client-ca.der -outform DER
+​
+rm test-*.*
