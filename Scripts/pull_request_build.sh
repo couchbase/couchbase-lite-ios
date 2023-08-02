e4f7161ac37aaee988401994bffe97f9b2cca3bc
Vlad Velicu <vlad_vel@yahoo.com>
2023-08-02 14:40:48 +0100

    CBL-4688: Jenkins PR Validation (EE) should run on both iOS and macOS (#3179)
    * build macos also

--- Scripts/pull_request_build.sh
+++ Scripts/pull_request_build.sh
@@ -6,11 +6,17 @@ if [ "$1" = "-enterprise" ]
     cd couchbase-lite-ios
 fi
 
+SCHEMES_MACOS=("CBL_EE_ObjC" "CBL_EE_Swift")
+
+for SCHEMES_MACOS in "${SCHEMES_MACOS[@]}"
+do
+  xcodebuild build -project CouchbaseLite.xcodeproj -scheme "$SCHEMES_MACOS" -destination "platform=macOS"
+done
 
 TEST_SIMULATOR=$(xcrun xctrace list devices 2>&1 | grep -oE 'iPhone.*?[^\(]+' | head -1 | sed 's/Simulator//g' | awk '{$1=$1;print}')
-SCHEMES=("CBL_EE_ObjC_Tests_iOS_App" "CBL_EE_Swift_Tests_iOS_App")
+SCHEMES_IOS=("CBL_EE_ObjC_Tests_iOS_App" "CBL_EE_Swift_Tests_iOS_App")
 
-for SCHEME in "${SCHEMES[@]}"
+for SCHEMES_IOS in "${SCHEMES_IOS[@]}"
 do
-  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEME" -sdk iphonesimulator -destination "platform=iOS Simulator,name=${TEST_SIMULATOR}"
+  xcodebuild test -project CouchbaseLite.xcodeproj -scheme "$SCHEMES_IOS" -sdk iphonesimulator -destination "platform=iOS Simulator,name=${TEST_SIMULATOR}"
 done
