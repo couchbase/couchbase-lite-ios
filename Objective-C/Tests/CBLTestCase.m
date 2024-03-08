9bf3bb7aad37703857025b7dc539f4848ce8f9cc
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-03-07 17:58:04 -0800

    Remove verbose log

--- Objective-C/Tests/CBLTestCase.m
+++ Objective-C/Tests/CBLTestCase.m
@@ -46,8 +46,6 @@
 
 - (void) setUp {
     [super setUp];
-        
-    CBLDatabase.log.console.level = kCBLLogLevelVerbose;
     
     [self deleteDBNamed: kDatabaseName error: nil];
     [self deleteDBNamed: kOtherDatabaseName error: nil];
