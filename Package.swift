22c37fb9eaef3859f969d2486e3666b32659aca8
Vlad Velicu <vlad.velicu@couchbase.com>
2024-03-12 15:42:49 +0000

    missed coma

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
             name: "CouchbaseLiteSwift",
             url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.6/couchbase-lite-swift_xc_community_3.1.6.zip",
             checksum: "749510257a61f41f1b1c6605d3025ed1d493ec5f6b236106b753202a6bf261d9"
-        )
-
+        ),
         .binaryTarget(
             name: "CouchbaseLiteSwift",
             url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.6/couchbase-lite-swift_xc_enterprise_3.1.6.zip",
