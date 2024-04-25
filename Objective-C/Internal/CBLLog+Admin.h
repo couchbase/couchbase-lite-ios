446cf2aa9a6d5fa7d634aa180e360656c62d6714
Vlad Velicu <vlad.velicu@couchbase.com>
2024-04-25 15:23:49 +0100

    fix typo

--- Objective-C/Internal/CBLLog+Admin.h
+++ Objective-C/Internal/CBLLog+Admin.h
@@ -1,8 +1,8 @@
 //
-//  CBLLogging+Admin.h
+//  CBLLogg+Admin.h
 //  CouchbaseLite
 //
-//  Copyright (c) 2018 Couchbase, Inc All rights reserved.
+//  Copyright (c) 2024 Couchbase, Inc All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
