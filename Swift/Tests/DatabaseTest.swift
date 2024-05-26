ad47a224029f172cf047f96c47e3e4c9a742b1f7
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-05-26 11:29:04 -0700

    Attempt to fix flaky tests
    I have noticed that recently the macOS agent on Github Action is much slower than before. I have added `expTimeout` variable for the default expectation timeout and increased a timeout to 20 seconds.

--- Swift/Tests/DatabaseTest.swift
+++ Swift/Tests/DatabaseTest.swift
@@ -1375,7 +1375,7 @@ class DatabaseTest: CBLTestCase {
         let q2 = QueryBuilder.select().from(ds)
         q2.addChangeListener { (ch) in change2.fulfill() }
         
-        wait(for: [change1, change2], timeout: 5.0)
+        wait(for: [change1, change2], timeout: expTimeout)
         
         try db.close()
     }
@@ -1396,7 +1396,7 @@ class DatabaseTest: CBLTestCase {
         let q2 = QueryBuilder.select().from(ds)
         q2.addChangeListener { (ch) in change2.fulfill() }
         
-        wait(for: [change1, change2], timeout: 5.0)
+        wait(for: [change1, change2], timeout: expTimeout)
         
         // Replicators:
         
@@ -1418,11 +1418,11 @@ class DatabaseTest: CBLTestCase {
         let stopped2 = expectation(description: "Stopped 2")
         startReplicator(r2, idleExpectation: idle2, stoppedExpectation: stopped2)
         
-        wait(for: [idle1, idle2], timeout: 5.0)
+        wait(for: [idle1, idle2], timeout: expTimeout)
         
         try! db.close()
         
-        wait(for: [stopped1, stopped2], timeout: 5.0)
+        wait(for: [stopped1, stopped2], timeout: expTimeout)
     }
     
     func startReplicator(_ replicator: Replicator, idleExpectation: XCTestExpectation, stoppedExpectation: XCTestExpectation) {
@@ -1452,11 +1452,11 @@ class DatabaseTest: CBLTestCase {
         let stopped2 = expectation(description: "Stopped 2")
         startReplicator(r2, idleExpectation: idle2, stoppedExpectation: stopped2)
               
-        wait(for: [idle1, idle2], timeout: 5.0)
+        wait(for: [idle1, idle2], timeout: expTimeout)
               
         try! db.close()
               
-        wait(for: [stopped1, stopped2], timeout: 5.0)
+        wait(for: [stopped1, stopped2], timeout: expTimeout)
     }
     
     #endif
