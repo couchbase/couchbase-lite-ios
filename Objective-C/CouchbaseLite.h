8b8f7cadf96aacb0d935a0c6ce0158f9ba612aa2
Vlad Velicu <vlad.velicu@couchbase.com>
2024-06-13 01:08:31 +0100

    add new headers to CouchbaseLite

--- Objective-C/CouchbaseLite.h
+++ Objective-C/CouchbaseLite.h
@@ -78,6 +78,7 @@ FOUNDATION_EXPORT const unsigned char CouchbaseLiteVersionString[];
 #import <CouchbaseLite/CBLQueryFunction.h>
 #import <CouchbaseLite/CBLQueryFullTextExpression.h>
 #import <CouchbaseLite/CBLQueryFullTextFunction.h>
+#import <CouchbaseLite/CBLQueryIndex.h>
 #import <CouchbaseLite/CBLQueryJoin.h>
 #import <CouchbaseLite/CBLQueryLimit.h>
 #import <CouchbaseLite/CBLQueryMeta.h>
@@ -111,6 +112,7 @@ FOUNDATION_EXPORT const unsigned char CouchbaseLiteVersionString[];
 #import <CouchbaseLite/CBLDatabaseEndpoint.h>
 #import <CouchbaseLite/CBLEncryptionKey.h>
 #import <CouchbaseLite/CBLIndexBuilder+Prediction.h>
+#import <CouchbaseLite/CBLIndexUpdater.h>
 #import <CouchbaseLite/CBLListenerAuthenticator.h>
 #import <CouchbaseLite/CBLListenerCertificateAuthenticator.h>
 #import <CouchbaseLite/CBLListenerPasswordAuthenticator.h>
