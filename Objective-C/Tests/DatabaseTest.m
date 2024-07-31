46ae809eb81f2a5ec3ed4df95062cbe4ee00fa18
Vlad Velicu <vlad.velicu@couchbase.com>
2024-07-31 18:00:27 +0300

    add tests

--- Objective-C/Tests/DatabaseTest.m
+++ Objective-C/Tests/DatabaseTest.m
@@ -2815,6 +2815,75 @@
     [token remove];
 }
 
+#pragma mark - Full Sync Option
+
+/** 
+ Test Spec for Database Full Sync Option https://github.com/couchbaselabs/couchbase-lite-api/blob/master/spec/tests/T0003-SQLite-Options.md
+ */
+
+/**
+ 1. TestSQLiteFullSyncConfig
+ Description
+    Test that the FullSync default is as expected and that it's setter and getter work.
+ Steps
+    1. Create a DatabaseConfiguration object.
+    2. Get and check the value of the FullSync property: it should be false.
+    3. Set the FullSync property true
+    4. Get the config FullSync property and verify that it is true
+    5. Set the FullSync property false
+    6. Get the config FullSync property and verify that it is false
+ */
+- (void) testSQLiteFullSyncConfig {
+    CBLDatabaseConfiguration* config = [[CBLDatabaseConfiguration alloc] init];
+    AssertFalse(config.isFullSync);
+    
+    config.isFullSync = true;
+    Assert(config.isFullSync);
+    
+    config.isFullSync = false;
+    AssertFalse(config.isFullSync);
+}
+
+/**
+ 2. TestDBWithFullSync
+ Description
+    Test that a Database respects the FullSync property
+ Steps
+    1. Create a DatabaseConfiguration object and set Full Sync false
+    2. Create a database with the config
+    3. Get the configuration object from the Database and verify that FullSync is false
+    4. Use c4db_config2 (perhaps necessary only for this test) to confirm that its config does not contain the kC4DB_DiskSyncFull flag
+    5. Set the config's FullSync property true
+    6. Create a database with the config
+    7. Get the configuration object from the Database and verify that FullSync is true
+    8. Use c4db_config2 to confirm that its config contains the kC4DB_DiskSyncFull flag
+ */
+- (void) testDBWithFullSync {
+    NSError* error;
+    NSString* dbName = @"fullsyncdb";
+    [CBLDatabase deleteDatabase: dbName inDirectory: self.directory error: nil];
+    AssertFalse([CBLDatabase databaseExists: dbName inDirectory: self.directory]);
+    
+    CBLDatabaseConfiguration* config = [[CBLDatabaseConfiguration alloc] init];
+    config.directory = self.directory;
+    CBLDatabase* db = [[CBLDatabase alloc] initWithName: dbName
+                                                 config: config
+                                                  error: &error];
+    AssertNil(error);
+    AssertNotNil(db, @"Couldn't open db: %@", error);
+    AssertFalse([db config].isFullSync);
+    [self closeDatabase: db];
+    
+    config.isFullSync = true;
+    db = [[CBLDatabase alloc] initWithName: dbName
+                                    config: config
+                                     error: &error];
+    AssertNil(error);
+    AssertNotNil(db, @"Couldn't open db: %@", error);
+    Assert([db config].isFullSync);
+    [self closeDatabase: db];
+}
+
 #pragma clang diagnostic pop
 
 @end
