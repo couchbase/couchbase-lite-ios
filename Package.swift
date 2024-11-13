227f530f331f6cf530e1bf563b778da3af1c4b43
Vlad Velicu <vlad.velicu@couchbase.com>
2024-11-13 12:48:17 +0000

    Update SPM and misc

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.2.0/couchbase-lite-swift_xc_community_3.2.0.zip",
-            checksum: "62c9f70038628822fa5d275b4c64c64f58abba6a5c178859f60160ee0e7baefd"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.2.1/couchbase-lite-swift_xc_community_3.2.1.zip",
+            checksum: "5791fbd02ef98faa1abda3c4de988addfe396879851139333c3dd149fba2aed4"
         )
     ]
 )
