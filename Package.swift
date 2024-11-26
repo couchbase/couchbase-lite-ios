02b6b01f3eda64b9e2fe26b25b5966e15b61096d
Vlad Velicu <vlad.velicu@couchbase.com>
2024-11-26 18:10:51 +0000

    3.1.10 SPM

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.9/couchbase-lite-swift_xc_community_3.1.9.zip",
-            checksum: "26fb83bd3c417d094f44e87ff8876842619a126f9707c1d834476c680702facb"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.10/couchbase-lite-swift_xc_community_3.1.10.zip",
+            checksum: "3005b52f55eb6426cdec361a1ffbaeab592f5eb1aeb81a66377ed9ab1a87dcbb"
         )
     ]
 )
