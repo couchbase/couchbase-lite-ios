c91140595dfccda2af8b79c9179163ea3b8d3379
Pasin Suriyentrakorn <pasin@couchbase.com>
2025-05-23 08:42:12 -0700

    Update per code review

--- Swift/CBLBridging.swift
+++ Swift/CBLBridging.swift
@@ -2,7 +2,7 @@
 //  CBLBridging.swift
 //  CouchbaseLite
 //
-//  Copyright (c) 2017 Couchbase, Inc All rights reserved.
+//  Copyright (c) 2025 Couchbase, Inc All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
