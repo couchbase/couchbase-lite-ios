7c468ca4766b5d98519573578ef31c0d05d48b8b
Vlad Velicu <vlad_vel@yahoo.com>
2025-07-17 13:33:56 +0100

    CBL-7146: userAgent should show correct information about the client type - Enterprise vs Community (#3415)
    now in line with .net

--- Scripts/get_repo_version.sh
+++ Scripts/get_repo_version.sh
@@ -9,9 +9,25 @@ GIT_BRANCH=`git rev-parse --symbolic-full-name HEAD | sed -e 's/refs\/heads\///'
 GIT_COMMIT=`git rev-parse HEAD || true`
 GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
 
+# EE repo info (if exists)
+TOP=`git rev-parse --show-toplevel`
+EE_DIR="$TOP/../couchbase-lite-ios-ee"
+if [[ -d "$EE_DIR" ]]; then
+  pushd "$EE_DIR" > /dev/null
+  GIT_BRANCH_EE=`git rev-parse --symbolic-full-name HEAD | sed -e 's/refs\/heads\///'`
+  GIT_COMMIT_EE=`git rev-parse HEAD || true`
+  GIT_DIRTY_EE=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
+  popd > /dev/null
+else
+  GIT_BRANCH_EE=""
+  GIT_COMMIT_EE=""
+  GIT_DIRTY_EE=""
+fi
+
 echo "static const char* const GitCommit = \"$GIT_COMMIT\"; " \
      "static const char* const GitBranch = \"$GIT_BRANCH\"; " \
-     "static const char* const GitDirty  = \"$GIT_DIRTY\";" \
+     "static const char* const GitCommitEE = \"$GIT_COMMIT_EE\"; " \
+     "static const char* const GitBranchEE = \"$GIT_BRANCH_EE\"; " \
      >"$OUTPUT_FILE".tmp
 
 if cmp --quiet "$OUTPUT_FILE" "$OUTPUT_FILE".tmp
