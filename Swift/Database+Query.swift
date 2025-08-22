33025297c31b2c5a58f6c147a19addd4404fbdbb
Vlad Velicu <vlad.velicu@couchbase.com>
2025-08-22 21:08:21 +0100

    Swift API and impl

--- Swift/Database+Query.swift
+++ Swift/Database+Query.swift
@@ -30,44 +30,4 @@ extension Database : QueryFactory {
     public func createQuery(_ query: String) throws -> Query {
         return try Query(database: self, expressions: query)
     }
-    
-    /// All index names.
-    @available(*, deprecated, message: "Use database.defaultCollection().indexes() instead.")
-    public var indexes: Array<String> { return impl.indexes }
-    
-    /// Creates an index wih the given name in the default collection. The index could be a value index or a full-text index.
-    /// The index name can be used later for deleting the index. Creating a new different index with an existing index name will
-    /// replace the old index. Creating the same index with the same name will be no-ops.
-    ///
-    /// - Parameters:
-    ///   - index: The index.
-    ///   - name: The index name.
-    /// - Throws: An error on a failure.
-    @available(*, deprecated, message: "Use database.defaultCollection().createIndex(:name:) instead.")
-    public func createIndex(_ index: Index, withName name: String) throws {
-        try impl.createIndex(index.toImpl(), withName: name)
-    }
-    
-    /// Creates an index in the default collection with the index config and the index name. The index config could be a value index or a full-text
-    /// index config. The index name can be used later for deleting the index.  Creating a new different index with an existing index name
-    /// will replace the old index. Creating the same index with the same name will be no-ops.
-    ///
-    /// - Parameters:
-    ///   - config: The index configuration
-    ///   - name: The index name.
-    /// - Throws: An error on a failure.
-    @available(*, deprecated, message: "Use database.defaultCollection().createIndex(:name:) instead.")
-    public func createIndex(_ config: IndexConfiguration, name: String) throws {
-        try impl.createIndex(withConfig: config.toImpl(), name: name)
-    }
-    
-    /// Deletes the index from the default collection by name.
-    ///
-    /// - Parameter name: The index name.
-    /// - Throws: An error on a failure.
-    @available(*, deprecated, message: "Use database.defaultCollection().deleteIndex(forName:) instead.")
-    public func deleteIndex(forName name: String) throws {
-        try impl.deleteIndex(forName: name)
-    }
-    
 }
