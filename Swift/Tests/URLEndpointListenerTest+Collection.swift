5d179450680ec69c296e20e11663cbc7ae076626
Pasin <pasin@couchbase.com>
2025-08-21 22:32:26 -0700

    Add repo test and blobs

--- Swift/Tests/URLEndpointListenerTest+Collection.swift
+++ Swift/Tests/URLEndpointListenerTest+Collection.swift
@@ -113,5 +113,84 @@ class URLEndpointListenerTest_Collection: URLEndpointListenerTest {
         try stopListener()
     }
     
-    // Note: testCreateListenerConfigWithEmptyCollection can't be tested due to fatalError
+    func testCBL7347() throws {
+        if !self.keyChainAccessAllowed { return }
+        
+        LogSinks.console = ConsoleLogSink(level: .info)
+        
+        let col1a = try self.db.createCollection(name: "colA", scope: "scopeA")
+        let col2a = try otherDB!.createCollection(name: "colA", scope: "scopeA")
+        
+        let blobs = ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "l1", "l2", "l3"]
+        var blobMap: [String: Data] = [:]
+        
+        let batchSize = 100
+        let totalDocs = 10000
+        for batchStart in stride(from: 0, to: totalDocs, by: batchSize) {
+            try autoreleasepool {
+                try col1a.database.inBatch {
+                    let batchEnd = min(batchStart + batchSize, totalDocs)
+                    NSLog("Saving : \(batchStart) ... \(batchEnd)")
+                    for i in batchStart..<batchEnd {
+                        let doc = MutableDocument(id: "doc-\(i)")
+                        doc.setString("Document #\(i)", forKey: "name")
+                        doc.setInt(i, forKey: "index")
+                        doc.setDate(Date(), forKey: "createdAt")
+                        
+                        let count = Int.random(in: 1...3)
+                        let chosenBlobs = blobs.shuffled().prefix(count)
+                        
+                        for (idx, blobName) in chosenBlobs.enumerated() {
+                            var data = blobMap[blobName]
+                            if data == nil {
+                                data = try dataFromResource(name: "blobs/\(blobName)", ofType: "jpg")
+                                blobMap[blobName] = data
+                            }
+                            if let data = data {
+                                let blob = Blob(contentType: "image/jpeg", data: data)
+                                doc.setBlob(blob, forKey: "image\(idx+1)")
+                            }
+                        }
+                        
+                        try col1a.save(document: doc)
+                    }
+                }
+            }
+        }
+        
+        NSLog("count : \(col1a.count)")
+        
+        let config = URLEndpointListenerConfiguration(collections: [col2a])
+        let listener = try startListener(withConfig: config)
+        
+        let collections = CollectionConfiguration.fromCollections([col1a])
+        var replicatorConfig = ReplicatorConfiguration(collections: collections, target: listener.localURLEndpoint)
+        replicatorConfig.continuous = true
+        replicatorConfig.pinnedServerCertificate = listener.tlsIdentity!.certs[0]
+        
+        let x = self.expectation(description: "stopped")
+
+        let replicator = Replicator.init(config: replicatorConfig)
+        _ = replicator.addChangeListener { (change) in
+            let status = change.status
+            if status.activity == .idle && status.progress.completed == status.progress.total {
+                replicator.stop()
+            }
+            
+            NSLog("\(status.progress.completed) / \(status.progress.total)")
+            
+            if (status.progress.completed > 40000) {
+                replicator.stop()
+            }
+            
+            if status.activity == .stopped {
+                x.fulfill()
+            }
+        }
+        
+        replicator.start()
+        wait(for: [x], timeout: 1000000)
+        
+        LogSinks.console = nil
+    }
 }
