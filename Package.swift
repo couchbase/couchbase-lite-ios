79f1583441027b16110e90b83251d769fe19785f
Pasin Suriyentrakorn <pasin@couchbase.com>
2025-12-15 09:05:39 -0800

    Update Swift Package for 3.3.1 (#3487)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.3.0/couchbase-lite-swift_xc_community_3.3.0.zip",
-            checksum: "933c16e353249bd40f8ff0b47c6496f863f0a33e5cd2de461dcd86c958b7c548"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.3.1/couchbase-lite-swift_xc_community_3.3.1.zip",
+            checksum: "1dc441c4a816fe320d6714cd2781560550b56579d37211798116741c02c5b92b"
         )
     ]
 )
