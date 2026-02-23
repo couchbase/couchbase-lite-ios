15b301c4f0faa1233e78eb5a690615002d8f0595
Pasin Suriyentrakorn <pasin@couchbase.com>
2026-02-23 14:11:18 -0800

    Update SPM to 3.3.2 (#3503)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.3.1/couchbase-lite-swift_xc_community_3.3.1.zip",
-            checksum: "1dc441c4a816fe320d6714cd2781560550b56579d37211798116741c02c5b92b"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.3.2/couchbase-lite-swift_xc_community_3.3.2.zip",
+            checksum: "eefb8205c2b19a95aea29c8071df2c631bf1b345c85ea6e2909d286f9cfa19da"
         )
     ]
 )
